d8f58ccdf284b94135ce797aa3a09d67906a0290
Joshua Kim <joshua0702@gmail.com>
2022-09-28 23:56:28 +0200

    remove redundant imports

--- src/AbstractAllowance.sol
+++ src/AbstractAllowance.sol
@@ -1,12 +1,9 @@
 pragma solidity ^0.8.13;
 
 import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
-import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import "openzeppelin-contracts/contracts/utils/math/SafeCast.sol";
 import "./interfaces/IAsset.sol";
 import "./interfaces/IManager.sol";
 import "./interfaces/IAccount.sol";
-import "forge-std/console2.sol";
 
 /**
  * @title Account
