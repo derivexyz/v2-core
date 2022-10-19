d5fa4a2aebbecc0c6538728fa4d91bb5f603ed67
Anton Cheng <antonassocareer@gmail.com>
2022-10-19 18:58:54 +0800

    gas: commit gas

---
+++ test/commitment/CommitmentGas.t.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "src/commitments/CommitmentBest.sol";
+
+contract CommitmentBestGas is Script {
+  uint ownAcc;
+  CommitmentBest commitment;
+
+  uint expiry;
+
+  function run() external {
+    vm.startBroadcast();
+
+    deployMockSystem();
+
+    // gas tests
+    uint gasBefore = gasleft();
+    commitment.commit(100, 1, 1);
+    uint gasAfter = gasleft();
+    console.log("gas commit#1", gasBefore - gasAfter);
+
+    gasBefore = gasleft();
+    commitment.commit(100, 2, 1);
+    gasAfter = gasleft();
+    console.log("gas commit#2", gasBefore - gasAfter);
+    
+    _commitMultiple(100);
+    vm.warp(block.timestamp + 10 minutes);
+
+    gasBefore = gasleft();
+    commitment.proccesQueue();
+    gasAfter = gasleft();
+    console.log("gas commit after process 100 in queue", gasBefore - gasAfter);
+
+    vm.stopBroadcast();
+  }
+
+  function _commitMultiple(uint count) internal {
+    for (uint i; i < count; i++) {
+      commitment.commit(104, uint16(i), 1);
+    }
+  }
+
+
+  function deployMockSystem() public {
+    commitment = new CommitmentBest();
+  }
+}
