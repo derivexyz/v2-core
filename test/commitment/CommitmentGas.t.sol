781aff4b9bc182e88e2edb9e1c1a3713b22b6cb7
Anton Cheng <antonassocareer@gmail.com>
2022-10-19 19:44:10 +0800

    chore: forge script

--- test/commitment/CommitmentGas.t.sol
+++ test/commitment/CommitmentGas.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 import "src/commitments/CommitmentBest.sol";
+import "src/commitments/CommitmentAverage.sol";
 
 contract CommitmentBestGas is Script {
   uint ownAcc;
@@ -45,5 +46,64 @@ contract CommitmentBestGas is Script {
 
   function deployMockSystem() public {
     commitment = new CommitmentBest();
+  }
+}
+
+
+contract CommitmentAvgGas is Script {
+  uint ownAcc;
+  CommitmentAverage commitment;
+
+  uint expiry;
+
+  function run() external {
+    vm.startBroadcast();
+
+    deployMockSystem();
+
+    // gas tests
+    uint gasBefore = gasleft();
+    commitment.commit(100, 1, 1);
+    uint gasAfter = gasleft();
+    console.log("gas commit#1", gasBefore - gasAfter);
+
+    gasBefore = gasleft();
+    commitment.commit(102, 2, 1);
+    gasAfter = gasleft();
+    console.log("gas commit#2", gasBefore - gasAfter);
+
+    gasBefore = gasleft();
+    commitment.commit(104, 3, 1);
+    gasAfter = gasleft();
+    console.log("gas commit#3", gasBefore - gasAfter);
+
+    vm.warp(block.timestamp + 5 minutes);
+
+    gasBefore = gasleft();
+    commitment.executeCommit(1, 1);
+    gasAfter = gasleft();
+    console.log("execute#1", gasBefore - gasAfter);
+    
+    gasBefore = gasleft();
+    commitment.executeCommit(2, 1);
+    gasAfter = gasleft();
+    console.log("execute#2", gasBefore - gasAfter);
+
+    // gasBefore = gasleft();
+    // commitment.executeCommit(3, 1);
+    // gasAfter = gasleft();
+    // console.log("execute#3", gasBefore - gasAfter);
+
+    vm.stopBroadcast();
+  }
+
+  function _commitMultiple(uint count) internal {
+    for (uint i; i < count; i++) {
+      commitment.commit(104, uint16(i), 1);
+    }
+  }
+
+  function deployMockSystem() public {
+    commitment = new CommitmentAverage();
   }
 }
