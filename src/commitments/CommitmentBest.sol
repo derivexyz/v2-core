27db98b6d5b73af007b904547d915e3bdc756525
Anton Cheng <antonassocareer@gmail.com>
2022-10-19 18:06:56 +0800

    chore: best ask or bid

--- src/commitments/CommitmentBest.sol
+++ src/commitments/CommitmentBest.sol
@@ -11,13 +11,22 @@ contract CommitmentBest {
   struct Commitment {
     uint16 bidVol;
     uint16 askVol;
+    uint16 nodeId;
     uint16 commitments;
     uint64 timestamp;
   }
 
   Commitment[256] public queue;
 
-  mapping(uint8 => mapping(uint16 => Commitment)) commitments;
+  uint8 lastUnprocessedIndex;
+  uint8 lastIndex;
+
+  uint16 currentBestBid;
+  uint16 currentBestAsk;
+  uint64 currentBestBidTimestamp;
+  uint64 currentBestAskTimestamp;
+
+  // mapping(uint8 => mapping(uint16 => Commitment)) commitments;
 
   uint16 constant RANGE = 5;
 
@@ -28,12 +37,60 @@ contract CommitmentBest {
     // todo: cannot double commit;
     // todo: check sender node id
 
-    _processQueue();
+    (, uint8 newIndex) = _processQueue();
 
-    (uint16 bidVol, uint16 askVol) = (vol - RANGE, vol + RANGE);
+    queue[newIndex] = Commitment(vol - RANGE, vol + RANGE, node, weight, uint64(block.timestamp));
+
+    lastIndex = newIndex;
   }
 
-  function _processQueue() internal {
+  function _processQueue() internal returns (uint8 lastProcessed, uint8 newIndex) {
     // get all commits more than 5 minutes
+    (uint8 cachedLastUnproccessed, uint8 cacheCurrentIndex) = (lastUnprocessedIndex, lastIndex);
+    newIndex = cacheCurrentIndex + 1;
+    if (newIndex == type(uint8).max) newIndex = 0;
+
+    if (cachedLastUnproccessed == cacheCurrentIndex) {
+      return (cachedLastUnproccessed, newIndex);
+    }
+
+    (uint16 cacheBestBid, uint16 cacheBestAsk, uint64 cacheBestBidTime, uint64 cacheBestAskTime) =
+      (currentBestBid, currentBestAsk, currentBestBidTimestamp, currentBestAskTimestamp);
+
+    (bool updateBid, bool updateAsk) = (false, false);
+    unchecked {
+      // let i overflow to 0
+      for (; cachedLastUnproccessed <= cacheCurrentIndex; cachedLastUnproccessed++) {
+        Commitment memory cache = queue[cachedLastUnproccessed];
+        
+        if (block.timestamp - cache.timestamp < 5 minutes) break;
+
+        if (cache.bidVol > cacheBestBid) {
+          updateBid = true;
+          cacheBestBid = cache.bidVol;
+          cacheBestBidTime = cache.timestamp;
+        }
+
+        if (cacheBestAsk == 0 || cache.askVol < cacheBestAsk) {
+          updateAsk = true;
+          cacheBestAsk = cache.askVol;
+          cacheBestAskTime = cache.timestamp;
+        }
+      }
+    }
+
+    if (updateBid) {
+      currentBestBid = cacheBestBid;
+      currentBestBidTimestamp = cacheBestBidTime;
+    }
+
+    if (updateAsk) {
+      currentBestAsk = cacheBestAsk;
+      currentBestAskTimestamp = cacheBestAskTime;
+    }
+
+    lastUnprocessedIndex = cachedLastUnproccessed;
+
+    return (cachedLastUnproccessed, newIndex);
   }
 }
---
+++ test/commitment/CommitBest.t.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console2.sol";
+
+import "src/commitments/CommitmentBest.sol";
+
+contract UNIT_CommitBest is Test {
+  CommitmentBest commitment;
+
+  constructor() {
+    commitment = new CommitmentBest();
+
+    vm.warp(block.timestamp + 1 days);
+  }
+
+  function testCanCommit() public {
+    commitment.commit(100, 1, 1);
+
+    commitment.commit(100, 2, 1);
+
+    commitment.commit(102, 3, 1);
+
+    commitment.commit(105, 4, 1);
+
+    vm.warp(block.timestamp + 10 minutes);
+
+    commitment.commit(102, 4, 1);
+
+    // (uint16 bidVol, uint16 askVol, uint16 totalCommitment,) = commitment.state(commitment.COLLECTING());
+    // assertEq(bidVol, 95);
+    // assertEq(askVol, 105);
+    // assertEq(totalCommitment, 1);
+
+    // // this will rotate again -> become only one in COLLECTING
+
+    // // firt one committing to pending
+    // commitment.commit(110, 1, 1);
+    // // commit another
+    // commitment.commit(116, 2, 1);
+    // (uint16 bidVol2, uint16 askVol2,,) = commitment.state(commitment.COLLECTING());
+
+    // assertEq(bidVol2, 113 - 5);
+    // assertEq(askVol2, 113 + 5);
+  }
+
+  // function testCanExecuteCommit() public {
+  //   commitment.commit(100, 1, 1);
+  //   // this will rotate again -> become only one in COLLECTING
+
+  //   // firt one committing to pending
+  //   commitment.commit(110, 1, 1);
+  //   commitment.commit(110, 2, 1);
+  //   commitment.commit(116, 3, 1);
+
+  //   vm.warp(block.timestamp + 10 minutes);
+  //   commitment.executeCommit(3, 1);
+  //   (uint16 bidVol, uint16 askVol, uint16 totalCommitment,) = commitment.state(commitment.PENDING());
+
+  //   // commitment.executeCommit(1, 1);
+  //   assertEq(bidVol, 105);
+  //   assertEq(askVol, 115);
+  //   assertEq(totalCommitment, 2);
+
+  //   // trigger another round
+  //   vm.warp(block.timestamp + 10 minutes);
+  //   commitment.commit(110, 1, 1);
+
+  //   (uint16 bidVolFinal, uint16 askVolFinal,,) = commitment.state(commitment.FINALIZED());
+  //   assertEq(bidVolFinal, 105);
+  //   assertEq(askVolFinal, 115);
+  //   //
+  // }
+}
