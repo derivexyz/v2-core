e305d5da5fc71d524858d7b57ca18d55e05f1d28
Anton Cheng <antonassocareer@gmail.com>
2022-12-05 06:59:54 +0800

    chore: outline for managers

---
+++ src/managers/PCRM/PCRMLiquidation.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "openzeppelin/token/ERC721/ERC721.sol";
+import "openzeppelin/utils/math/SafeCast.sol";
+
+/**
+ * @title PCRMLiquidation
+ * @author Lyra
+ * @notice Liquidation module for PCRM (Partial Collateral Risk Manager). In charge of:
+ *         1. start an auction for a underwater account
+ *         2. determine what part of the portfolio should be put on liquidation
+ */
+contract PCRMLiquidation {
+
+}
