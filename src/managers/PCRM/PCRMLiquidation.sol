4c8020b846010c4d83a9a457aadd43d5080d39ef
Anton Cheng <antonassocareer@gmail.com>
2022-12-07 07:28:41 +0800

    chore: update outline

--- src/managers/PCRM/PCRMLiquidation.sol
+++ src/managers/PCRM/PCRMLiquidation.sol
@@ -28,51 +28,59 @@ contract PCRMLiquidation {
   //    External Functions   //
   /////////////////////////////
 
+  /**
+   * @notice
+   * @dev 
+   * @param accountId account to be flagged
+   */
   function flagLiquidation(uint accountId) external {
     // check that account is underwater
 
     // request the manager to transfer the account ownership to the liquidation module
 
     // start an auction
+
+    // incentive to caller?
   }
 
   /**
-   * @notice participate in the auction and liquidate an account
+   * @notice participate in the auction to liquidate an account
+   * @dev the caller need to grant access to ths contrat to move its balance in Account
+   * @param auctionId auction id
+   * @param fromAccount account to receive debt and cash
+   * @param amountPercentage amount to liquidate
+   * @param minUsdc min amount to receive
    */
-  function liquidate(uint auctionId, uint fromAccount, uint amountPercentage, uint maxUsdc) external {
+  function liquidate(uint auctionId, uint fromAccount, uint amountPercentage, uint minUsdc) external {
     // check fromAccount access
 
     // check auction state
 
-    // caculate price based on timestamp
+    // caculate curr price based on timestamp
 
     // calculate f base on portfolio and price
 
-    // trigger transfer on Account
+    // trigger transfer on Account (need funds from security module if auctioning account is far underwater)
 
     // if the auction ends, transfer the account back to original owner
 
     // update auction id state
   }
 
-  /**
-   * @notice if the auction time has passed and no one is willing to take the portfolio
-   *         anyone can use this function to trigger the bail out pool to take the position.
-   */
-  function bailOutPorfolio(uint auctionid) external {
-    // check auction state
-
-    // tansfer whole account to the bail out pool
-  }
-
   ////////////////////
   //    Internal    //
   ////////////////////
 
+  /**
+   * @dev description: price calculation
+   * @param accountId account id
+   */
   function _startAuction(uint accountId) internal returns (uint auctionId) {
     // get the account detail
 
     // calculate init price to sell the portfolio (intrinsic value of all long and short)
+
+    // calculate end price to sell the portfolio (assuming worst case: 300 vol for short & 0 for long)
 
     // update auction id
 
