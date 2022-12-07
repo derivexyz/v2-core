27c362b97e3da1e9df8bc1097e29fc19fc80c4eb
Anton Cheng <antonassocareer@gmail.com>
2022-12-08 06:45:18 +0800

    build: proto-1 repo structure (#32)
    ## Summary
    
    * create folders structure for proto-1 
    * test folder structures for proto-1
    * draft outline for liquidation module
    
    ## Details
    
    - `risk-managers` folder for all managers
    - `assets` folder for all assets
    - `liquidation/PCRMLiquidation.sol` for liquidation module
    - `assets/Lending.sol` for lending cash
    
    ## Checklist
    
    Ensure you completed **all of the steps** below before submitting your
    pull request:
    
    - [x] Add
    [natspec](https://docs.soliditylang.org/en/latest/natspec-format.html)
    for all functions / parameters
    - [x] Ran `forge snapshot`
    - [x] Ran `forge fmt`
    - [x] Ran `forge test`
    - [x] 100% test coverage on code changes

---
+++ src/assets/Lending.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "openzeppelin/token/ERC20/IERC20.sol";
+import "openzeppelin/utils/math/SafeCast.sol";
+
+/**
+ * @title
+ * @dev
+ * @author lyra
+ */
+contract Lending {}
