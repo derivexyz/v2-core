3bb52c5aeff44f7316c05e5b8f1d02473d35d65e
Joshua Kim <joshua0702@gmail.com>
2022-12-08 16:54:34 -0800

    initial feed test scaffolds

--- test/feeds/unit-tests/TestChainlinkSpotFeeds.sol
+++ test/feeds/unit-tests/TestChainlinkSpotFeeds.sol
@@ -2,7 +2,39 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-
+import "test/feeds/mocks/MockV3Aggregator.sol";
+import "src/feeds/ChainlinkSpotFeeds.sol";
 contract TestChainlinkSpotFeeds is Test {
-  function setUp() public {}
+  ChainlinkSpotFeeds spotFeeds;
+  MockV3Aggregator aggregator1;
+  MockV3Aggregator aggregator2;
+
+  function setUp() public {
+    aggregator1 = new MockV3Aggregator(8, 1000e18);
+    aggregator2 = new MockV3Aggregator(18, 10000e18);
+    spotFeeds = new ChainlinkSpotFeeds();
+  }
+  
+  //////////////////
+  // Adding Feeds //
+  //////////////////
+
+  function testAddMultipleFeeds() {}
+
+  ////////////////////////
+  // Getting Spot Price //
+  ////////////////////////
+
+  function testGetSpotWithFeedId() {}
+
+  function testGetSpotWithTradingPair() {}
+
+  //////////////////////////
+  // Getting Feed Details //
+  //////////////////////////
+
+  function getFeedId() {}
+
+  function getTradingPair() {}
+
 }
