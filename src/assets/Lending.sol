ac3ed4c3dd11a5435f773d08fce985c65006b661
Anton Cheng <antonassocareer@gmail.com>
2022-12-09 08:19:58 +0800

    feat (Lending): asset hook (#43)
    ## Summary
    
    * Implement draft for `handleAdjustment` and `handleManagerChange`
    * provide structure for `Lending`, including some external / internal /
    modifier / error sections
    
    ## Detail
    * `handleAdjustment` is currently guarded with `onlyAccount` because I
    assume there will be other state updates like last update timestamp for
    an account, and we might not want anyone to mess up these variables by
    calling this function but not actually invoke any trade in the account.
    
    ## Todo
    - [x] Draft implementation for `handleAdjustment`, `handleManagerChange`
    - [x] Unit tests for full coverage
    
    ## Checklist
    
    Ensure you completed **all of the steps** below before submitting your
    pull request:
    
    - [x] Add
    [natspec](https://docs.soliditylang.org/en/latest/natspec-format.html)
    for all functions / parameters
    - [x] Ran `forge snapshot`
    - [x] Ran `forge fmt`
    - [x] Ran `forge test`
    - [x] [Triage Slither issues](../README.md#triage-issues), and post
    uncertain ones in the PR
    - [x] 100% test coverage on code changes
    
    ### Slither Issues (Optional)
    
    If you're unsure about a new issue reported by Slither, copy them here
    so others can verify as well.

--- src/assets/Lending.sol
+++ src/assets/Lending.sol
@@ -3,27 +3,52 @@ pragma solidity ^0.8.13;
 
 import "openzeppelin/token/ERC20/IERC20.sol";
 import "openzeppelin/utils/math/SafeCast.sol";
-
+import "synthetix/Owned.sol";
 import "../interfaces/IAsset.sol";
 
 /**
- * @title cash asset with built-in lending feature
- * @dev
+ * @title cash asset with built-in lending feature.
+ * @dev   user can deposit USDC and credit this cash asset into their account
+ *        users can borrow cash by having a negative balance in their account (if allowed by manager)
  * @author Lyra
  */
-contract Lending is IAsset {
-
+contract Lending is Owned, IAsset {
+  ///@dev account contract address
   address public immutable account;
 
+  /////////////////////////
+  //   State Variables   //
+  /////////////////////////
+
+  ///@dev borrow index
+  uint public borrowIndex;
+
+  ///@dev supply index
+  uint public supplyIndex;
+
+  ///@dev last timestamp that the interest is accrued
+  uint public lastTimestamp;
+
   ///@dev whitelisted managers
-  mapping (address => bool) public whitelistedManager;
+  mapping(address => bool) public whitelistedManager;
 
   ////////////////
   //   Errors   //
   ////////////////
 
-  /// @dev emited when user trying to upgrade to a unknown manager
+  /// @dev caller is not account
+  error LA_NotAccount();
+
+  /// @dev revert when user trying to upgrade to a unknown manager
   error LA_UnknownManager();
+
+  ///////////////////
+  //   Modifiers   //
+  ///////////////////
+  modifier onlyAccount() {
+    if (msg.sender != account) revert LA_NotAccount();
+    _;
+  }
 
   /////////////////////
   //   Constructor   //
@@ -39,9 +64,10 @@ contract Lending is IAsset {
 
   /**
    * @notice triggered when an adjustment is triggered on the asset balance
-   * @dev    the function will imply interest rate and modify the final balance.
-   * @param manager the manager contract that will verify the end state. Should verify if this is a trusted manager
-   * @param caller the msg.sender that initiate the transfer, can assume to be a address authorized by user
+   * @dev    we imply interest rate and modify the final balance. final balance can be positive or negative.
+   * @param adjustment details about adjustment, containing account, subId, amount
+   * @param preBalance balance before adjustment
+   * @param manager the manager contract that will verify the end state
    * @return finalBalance the final balance to be recorded in the account
    * @return needAllowance if this adjustment should require allowance from non-ERC721 approved initiator
    */
@@ -49,16 +75,57 @@ contract Lending is IAsset {
     AccountStructs.AssetAdjustment memory adjustment,
     int preBalance,
     IManager manager,
-    address caller
-  ) external returns (int finalBalance, bool needAllowance) {
+    address /*caller*/
+  ) external onlyAccount returns (int finalBalance, bool needAllowance) {
+    // todo: verify manager
 
+    // accrue interest rate
+    _accrueInterest();
+
+    // todo: accrue interest on prebalance
+
+    // finalBalance can go positive or negative
+    finalBalance = preBalance + adjustment.amount;
+
+    // need allowance if trying to deduct balance
+    needAllowance = adjustment.amount < 0;
   }
 
   /**
    * @notice triggered when a user wants to migrate an account to a new manager
    * @dev block update with non-whitelisted manager
    */
-  function handleManagerChange(uint /*accountId*/, IManager newManager) external {
-    if (!whitelistedManager[newManager]) revert LA_UnknownManager();
+  function handleManagerChange(uint, /*accountId*/ IManager newManager) external view {
+    if (!whitelistedManager[address(newManager)]) revert LA_UnknownManager();
+  }
+
+  //////////////////////////////
+  //   Owner-only Functions   //
+  //////////////////////////////
+
+  /**
+   * @notice whitelist or un-whitelist a manager
+   * @param _manager manager address
+   * @param _whitelisted true to whitelist
+   */
+  function setWhitelistManager(address _manager, bool _whitelisted) external onlyOwner {
+    whitelistedManager[_manager] = _whitelisted;
+  }
+
+  ////////////////////////////
+  //   External Functions   //
+  ////////////////////////////
+
+  ////////////////////////////
+  //   Internal Functions   //
+  ////////////////////////////
+
+  /**
+   * @dev update interest rate
+   */
+  function _accrueInterest() internal {
+    //todo: actual interest updates
+
+    lastTimestamp = block.timestamp;
   }
 }
