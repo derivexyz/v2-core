ab72bab4ba6d1239dece24710beb0ef32ea4e774
Lyndon Chang <lyndnc99@gmail.com>
2022-12-14 16:46:58 +1100

    build: InterestRateModel based off compound/mocks

---
+++ src/assets/InterestRateModel.sol
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "openzeppelin/token/ERC20/IERC20.sol";
+import "openzeppelin/token/ERC20/extensions/IERC20Metadata.sol";
+import "openzeppelin/token/ERC20/utils/SafeERC20.sol";
+import "openzeppelin/utils/math/SafeCast.sol";
+import "synthetix/Owned.sol";
+import "../libraries/DecimalMath.sol";
+import "../libraries/FixedPointMathLib.sol";
+import "../interfaces/IAsset.sol";
+import "../interfaces/IAccount.sol";
+import "../interfaces/IInterestRateModel.sol";
+
+/**
+ * @title Modeled off of Compound's JumpRateModel and Aave's compounding model
+ * @author Lyra
+ */
+contract InterestRateModel is IInterestRateModel, Owned {
+  using DecimalMath for uint;
+  using SafeCast for uint;
+
+  /////////////////////
+  // State Variables //
+  /////////////////////
+
+  ///@dev The approximate number of seconds per year
+  uint public constant SECONDS_PER_YEAR = 31536000;
+
+  ///@dev The base yearly interest rate which is the y-intercept when utilization rate is 0
+  uint public baseRatePerYear;
+
+  ///@dev The multiplier of utilization rate that gives the slope of the interest rate
+  uint public multiplier;
+  
+  ///@dev The multiplier after hitting a specified utilization point
+  uint public jumpMultiplier;
+
+  ///@dev The utilization point at which the jump multiplier is applied
+  uint public kink;
+
+  ////////////////////////
+  //    Constructor     //
+  ////////////////////////
+
+  /**
+   * @notice Construct an interest rate model
+   * @param baseRatePerYear_ The approximate target base APR, as a mantissa (scaled by BASE)
+   * @param multiplier_ The rate of increase in interest rate wrt utilization (scaled by BASE)
+   * @param jumpMultiplier_ The multiplierPerBlock after hitting a specified utilization point
+   * @param kink_ The utilization point at which the jump multiplier is applied
+   */
+  constructor(uint baseRatePerYear_, uint multiplier_, uint jumpMultiplier_, uint kink_) {
+    baseRatePerYear = baseRatePerYear_;
+    multiplier = multiplier_;
+    jumpMultiplier = jumpMultiplier_;
+    kink = kink_;
+
+    emit InterestRateParamsSet(baseRatePerYear, multiplier, jumpMultiplier, kink);
+  }
+
+  //////////////////////////////
+  //   Owner-only Functions   //
+  //////////////////////////////
+  
+  /**
+   * @notice Allows owner to set the interest rate parameters
+   */
+  function setInterestRateParams(uint _baseRatePerYear, uint _multiplier, uint _jumpMultiplier, uint _kink) external onlyOwner {
+    baseRatePerYear = _baseRatePerYear;
+    multiplier = _multiplier;
+    jumpMultiplier = _jumpMultiplier;
+    kink = _kink;
+
+    emit InterestRateParamsSet(_multiplier, _baseRatePerYear, _jumpMultiplier, _kink);
+  }
+
+  ////////////////////////////
+  //   External Functions   //
+  ////////////////////////////
+
+  /**
+   * @notice Function to calculate the interest using a compounded interest rate formula
+   *         P_0 * e ^(rt) = Principal with accrued interest
+   *
+   * @param elapsedTime seconds since last interest accrual
+   * @param cash underlying ERC20 balance
+   * @param borrows total outstanding debt
+   * @return InterestFactor : e^(rt) - 1
+   */
+  function getBorrowInterestFactor(uint elapsedTime, uint cash, uint borrows) external view override returns (uint) {
+    uint r = getBorrowRate(cash, borrows);
+    return FixedPointMathLib.exp((elapsedTime * r / SECONDS_PER_YEAR).toInt256()) - DecimalMath.UNIT;
+  }
+
+  /**
+   * @notice Calculates the current borrow rate per block, with the error code expected by the market
+   * @param cash The amount of cash in the market
+   * @param borrows The amount of borrows in the market
+   * @return The borrow rate percentage per block as a mantissa (scaled by BASE)
+   */
+  function getBorrowRate(uint cash, uint borrows) public view override returns (uint) {
+    uint util = utilizationRate(cash, borrows);
+
+    if (util <= kink) {
+      return util.multiplyDecimal(multiplier) + baseRatePerYear;
+    } else {
+      uint normalRate = kink.multiplyDecimal(multiplier) + baseRatePerYear;
+      uint excessUtil = util - kink;
+      return excessUtil.multiplyDecimal(jumpMultiplier) + normalRate;
+    }
+  }
+
+  /**
+   * @notice Calculates the current supply rate per block
+   * @param cash The amount of cash in the market
+   * @param borrows The amount of borrows in the market
+   * @param supply The amount of borrows in the market
+   * @param feeFactor The current reserve factor for the market
+   * @return The supply rate percentage per block as a mantissa (scaled by BASE)
+   */
+  function getSupplyRate(uint cash, uint borrows, uint supply, uint feeFactor) public view override returns (uint) {
+    uint oneMinusReserveFactor = DecimalMath.UNIT - feeFactor;
+    uint borrowRate = getBorrowRate(cash, borrows);
+    uint ratePostReserve = borrowRate.multiplyDecimal(oneMinusReserveFactor);
+    return ratePostReserve.multiplyDecimal(borrows).divideDecimal(supply);
+  }
+
+  /**
+   * @notice Calculates the utilization rate of the market: `borrows / cash`
+   * @param cash The amount of cash in the market
+   * @param borrows The amount of borrows in the market
+   * @return The utilization rate as a mantissa between [0, BASE]
+   */
+  function utilizationRate(uint cash, uint borrows) public pure returns (uint) {
+    // Utilization rate is 0 when there are no borrows
+    if (borrows == 0) {
+      return 0;
+    }
+
+    return borrows.divideDecimal(cash);
+  }
+
+  ////////////
+  // Events //
+  ////////////
+
+  ///@dev Emitted when interest rate parameters are set
+  event  InterestRateParamsSet(uint baseRatePerYear, uint multiplier, uint jumpMultiplier, uint kink);
+}
