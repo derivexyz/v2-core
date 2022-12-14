858d8a650e1b5828c20a5fdfd827d3df6c261ca1
Lyndon Chang <lyndnc99@gmail.com>
2022-12-14 14:51:44 +1100

    test: tests for totalSupply/borrow

--- src/interfaces/IAllowances.sol
+++ src/interfaces/IAllowances.sol
@@ -5,35 +5,61 @@ import "./IAsset.sol";
 
 // For full documentation refer to src/Allowances.sol";
 interface IAllowances {
-  ///////////
-  // Views //
-  ///////////
+    ///////////
+    // Views //
+    ///////////
 
-  function positiveSubIdAllowance(uint accountId, address owner, IAsset asset, uint subId, address spender)
-    external
-    view
-    returns (uint);
+    function positiveSubIdAllowance(
+        uint256 accountId,
+        address owner,
+        IAsset asset,
+        uint256 subId,
+        address spender
+    )
+        external
+        view
+        returns (uint256);
 
-  function negativeSubIdAllowance(uint accountId, address owner, IAsset asset, uint subId, address spender)
-    external
-    view
-    returns (uint);
+    function negativeSubIdAllowance(
+        uint256 accountId,
+        address owner,
+        IAsset asset,
+        uint256 subId,
+        address spender
+    )
+        external
+        view
+        returns (uint256);
 
-  function positiveAssetAllowance(uint accountId, address owner, IAsset asset, address spender)
-    external
-    view
-    returns (uint);
+    function positiveAssetAllowance(
+        uint256 accountId,
+        address owner,
+        IAsset asset,
+        address spender
+    )
+        external
+        view
+        returns (uint256);
 
-  function negativeAssetAllowance(uint accountId, address owner, IAsset asset, address spender)
-    external
-    view
-    returns (uint);
+    function negativeAssetAllowance(
+        uint256 accountId,
+        address owner,
+        IAsset asset,
+        address spender
+    )
+        external
+        view
+        returns (uint256);
 
-  ////////////
-  // Errors //
-  ////////////
+    ////////////
+    // Errors //
+    ////////////
 
-  error NotEnoughSubIdOrAssetAllowances(
-    address caller, uint accountId, int amount, uint subIdAllowance, uint assetAllowance
-  );
+    error NotEnoughSubIdOrAssetAllowances(
+        address caller,
+        uint256 accountId,
+        int256 amount,
+        uint256 subIdAllowance,
+        uint256 assetAllowance
+    );
 }
--- src/libraries/AssetDeltaLib.sol
+++ src/libraries/AssetDeltaLib.sol
@@ -9,63 +9,88 @@ import "src/interfaces/IAsset.sol";
  * @notice util functions for AssetDeltaLib operations
  */
 library AssetDeltaLib {
-  /// @dev too many deltas
-  error DL_DeltasTooLong();
+    /// @dev too many deltas
+    error DL_DeltasTooLong();
 
-  /**
-   * @notice apply delta to the AssetDeltaArrayCache.deltas array.
-   * @dev if this is an asset never seen before, add to the accountDelta.deltas array
-   *      if this is an asset seen before, update the accountDelta.deltas entry
-   * @dev will revert if the delta array is already full (100 entries);
-   *
-   */
-  function addToAssetDeltaArray(
-    AccountStructs.AssetDeltaArrayCache memory cache,
-    AccountStructs.AssetDelta memory delta
-  ) internal pure {
-    for (uint i; i < cache.deltas.length;) {
-      if (cache.deltas[i].asset == delta.asset && cache.deltas[i].subId == delta.subId) {
-        cache.deltas[i].delta += delta.delta;
-        return;
-      } else if (cache.deltas[i].asset == IAsset(address(0)) && cache.deltas[i].subId == 0) {
-        // find the first empty element, write information
-        cache.deltas[i] = delta;
-        unchecked {
-          cache.used += 1;
+    /**
+     * @notice apply delta to the AssetDeltaArrayCache.deltas array.
+     * @dev if this is an asset never seen before, add to the accountDelta.deltas array
+     *      if this is an asset seen before, update the accountDelta.deltas entry
+     * @dev will revert if the delta array is already full (100 entries);
+     *
+     */
+    function addToAssetDeltaArray(
+        AccountStructs.AssetDeltaArrayCache memory cache,
+        AccountStructs.AssetDelta memory delta
+    )
+        internal
+        pure
+    {
+        for (uint256 i; i < cache.deltas.length;) {
+            if (
+                cache.deltas[i].asset
+                    == delta.asset
+                    && cache.deltas[i].subId
+                    == delta.subId
+            ) {
+                cache.deltas[i].delta += delta.delta;
+                return;
+            } else if (
+                cache.deltas[i].asset
+                    == IAsset(address(0))
+                    && cache.deltas[i].subId
+                    == 0
+            ) {
+                // find the first empty element, write information
+                cache.deltas[i] = delta;
+                unchecked {
+                    cache.used += 1;
+                }
+                return;
+            }
+
+            unchecked {
+                i++;
+            }
         }
-        return;
-      }
-
-      unchecked {
-        i++;
-      }
+        revert DL_DeltasTooLong();
     }
-    revert DL_DeltasTooLong();
-  }
 
-  function getDeltasFromArrayCache(AccountStructs.AssetDeltaArrayCache memory cache)
-    internal
-    pure
-    returns (AccountStructs.AssetDelta[] memory)
-  {
-    AccountStructs.AssetDelta[] memory deltas = new AccountStructs.AssetDelta[](cache.used);
-    for (uint i; i < deltas.length;) {
-      deltas[i] = cache.deltas[i];
+    function getDeltasFromArrayCache(
+        AccountStructs.AssetDeltaArrayCache memory cache
+    )
+        internal
+        pure
+        returns (AccountStructs.AssetDelta[] memory)
+    {
+        AccountStructs.AssetDelta[] memory deltas =
+            new AccountStructs.AssetDelta[](cache.used);
+        for (uint256 i; i < deltas.length;) {
+            deltas[i] = cache.deltas[i];
 
-      unchecked {
-        i++;
-      }
+            unchecked {
+                i++;
+            }
+        }
+        return deltas;
     }
-    return deltas;
-  }
 
-  function getDeltasFromSingleAdjustment(IAsset asset, uint subId, int delta)
-    internal
-    pure
-    returns (AccountStructs.AssetDelta[] memory)
-  {
-    AccountStructs.AssetDelta[] memory deltas = new AccountStructs.AssetDelta[](1);
-    deltas[0] = AccountStructs.AssetDelta({asset: asset, subId: uint96(subId), delta: delta});
-    return deltas;
-  }
+    function getDeltasFromSingleAdjustment(
+        IAsset asset,
+        uint256 subId,
+        int256 delta
+    )
+        internal
+        pure
+        returns (AccountStructs.AssetDelta[] memory)
+    {
+        AccountStructs.AssetDelta[] memory deltas =
+            new AccountStructs.AssetDelta[](1);
+        deltas[0] = AccountStructs.AssetDelta({
+            asset: asset,
+            subId: uint96(subId),
+            delta: delta
+        });
+        return deltas;
+    }
 }
