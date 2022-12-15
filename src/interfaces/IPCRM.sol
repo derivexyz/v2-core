92da7824f838118031d187333306370fc38314ad
Joshua Kim <46257136+joshpwrk@users.noreply.github.com>
2022-12-15 15:19:47 -0800

    Feat/initial pcrm (#55)
    ## Summary
    
    Initial scaffolding required for the PCRM.
    
    ## Details
    
    - Follows mech spec-doc on calculating margin for PCRM
    - Math details ignored in this scaffold
    - settlement price is handled by option
    - option subIds are encoded 96bit unsigned integers (next feature)
    - actual cash transfer for settlement is handled by the manager
    - The option asset and lending asset are both immutable in the manager
    contract
    - Auction integration follows latest spec from Sean (e.g. no need for
    max proportion)
    
    ## Todo
    
    - [x] Scaffold of `PCRM`
    - [x] Auction related external functions
    - [x] Scaffold of `Option`
    - [x] Unit tests
    
    ## Checklist
    
    Ensure you completed **all of the steps** below before submitting your
    pull request:
    
    - [x] Add
    [natspec](https://docs.soliditylang.org/en/latest/natspec-format.html)
    for all functions / parameters
    - [x] Ran `forge snapshot`
    - [x] Ran `forge fmt`
    - [x] Ran `forge test`
    - [x] [Triage Slither issues](../README.md#triage-issues), and post
    uncertain ones in the PR
    - [x] 100% test coverage on code changes
    
    ### Slither Issues (Optional)
    
    If you're unsure about a new issue reported by Slither, copy them here
    so others can verify as well.

---
+++ src/interfaces/IPCRM.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+/**
+ * @title PartialCollateralRiskManager
+ * @author Lyra
+ * @notice Risk Manager that controls transfer and margin requirements
+ */
+interface IPCRM {
+  struct ExpiryHolding {
+    uint expiry;
+    StrikeHolding[] strikes;
+  }
+
+  struct StrikeHolding {
+    uint64 strike;
+    int64 calls;
+    int64 puts;
+    int64 forwards;
+  }
+
+  function getSortedHoldings(uint accountId) external view returns (ExpiryHolding[] memory expiryHoldings, int cash);
+
+  function executeBid(uint accountId, uint liquidatorId, uint portion, uint cashAmount)
+    external
+    returns (int finalInitialMargin, ExpiryHolding[] memory, int cash);
+}
---
+++ src/risk-managers/PCRM.sol
@@ -0,0 +1,272 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "src/interfaces/IManager.sol";
+import "src/interfaces/IAccount.sol";
+import "src/interfaces/ISpotFeeds.sol";
+import "src/interfaces/IDutchAuction.sol";
+import "src/assets/Lending.sol";
+import "src/assets/Option.sol";
+import "synthetix/Owned.sol";
+
+/**
+ * @title PartialCollateralRiskManager
+ * @author Lyra
+ * @notice Risk Manager that controls transfer and margin requirements
+ */
+contract PCRM is IManager, Owned {
+  /**
+   * INITIAL: margin required for trade to pass
+   * MAINTENANCE: margin required to prevent liquidation
+   */
+  enum MarginType {
+    INITIAL,
+    MAINTENANCE
+  }
+
+  struct ExpiryHolding {
+    /// timestamp of expiry for all strike holdings
+    uint expiry;
+    /// array of strike holding details
+    StrikeHolding[] strikes;
+  }
+
+  struct StrikeHolding {
+    /// strike price of held options
+    uint64 strike;
+    /// number of calls held
+    int64 calls;
+    /// number of puts held
+    int64 puts;
+    /// number of forwards held
+    int64 forwards;
+  }
+
+  ///////////////
+  // Variables //
+  ///////////////
+
+  /// @dev asset used in all settlements and denominates margin
+  IAccount public immutable account;
+
+  /// @dev spotFeeds that determine staleness and return prices
+  ISpotFeeds public spotFeeds;
+
+  /// @dev asset used in all settlements and denominates margin
+  Lending public immutable lending;
+
+  /// @dev reserved option asset
+  Option public immutable option;
+
+  /// @dev dutch auction contract used to auction liquidatable accounts
+  IDutchAuction public immutable dutchAuction;
+
+  ////////////
+  // Events //
+  ////////////
+
+  ///////////////
+  // Modifiers //
+  ///////////////
+
+  modifier onlyAuction() {
+    if (msg.sender != address(dutchAuction)) {
+      revert PCRM_OnlyAuction(msg.sender, address(dutchAuction));
+    }
+    _;
+  }
+
+  ////////////////////////
+  //    Constructor     //
+  ////////////////////////
+
+  constructor(address account_, address spotFeeds_, address lending_, address option_, address auction_) Owned() {
+    account = IAccount(account_);
+    spotFeeds = ISpotFeeds(spotFeeds_);
+    lending = Lending(lending_);
+    option = Option(option_);
+    dutchAuction = IDutchAuction(auction_);
+  }
+
+  ///////////////////
+  // Account Hooks //
+  ///////////////////
+
+  /**
+   * @notice Ensures asset is valid and initial margin is met.
+   * @param accountId Account for which to check trade.
+   */
+  function handleAdjustment(uint accountId, address, AccountStructs.AssetDelta[] memory, bytes memory)
+    public
+    view
+    override
+  {
+    // todo: whitelist check
+
+    // PCRM calculations
+    ExpiryHolding[] memory expiries = _sortHoldings(account.getAccountBalances(accountId));
+    _calcMargin(expiries, MarginType.INITIAL);
+  }
+
+  /**
+   * @notice Ensures new manager is valid.
+   * @param accountId Account for which to check trade.
+   * @param newManager IManager to change account to.
+   */
+  function handleManagerChange(uint accountId, IManager newManager) external {
+    // todo: nextManager whitelist check
+  }
+
+  //////////////////
+  // Liquidations //
+  //////////////////
+
+  /**
+   * @notice Confirm account is liquidatable and puts up for dutch auction.
+   * @param accountId Account for which to check trade.
+   */
+  function checkAndStartLiquidation(uint accountId) external {
+    dutchAuction.startAuction(accountId);
+    // todo: check that account is liquidatable / freeze account / call out to auction contract
+  }
+
+  /**
+   * @notice Transfers portion of account to the liquidator.
+   *         Transfers cash to the liquidated account.
+   * @dev Auction contract can decide to either:
+   *      - revert / process bid
+   *      - continue / complete auction
+   * @param accountId ID of account which is being liquidated.
+   * @param liquidatorId Liquidator account ID.
+   * @param portion Portion of account that is requested to be liquidated.
+   * @param cashAmount Cash amount liquidator is offering for portion of account.
+   * @return postExecutionInitialMargin InitialMargin of account after portion is liquidated.
+   * @return expiryHoldings Sorted array of option holdings used to recompute new auction bounds
+   * @return cash Amount of cash held or borrowed in account
+   */
+  function executeBid(uint accountId, uint liquidatorId, uint portion, uint cashAmount)
+    external
+    onlyAuction
+    returns (int postExecutionInitialMargin, ExpiryHolding[] memory, int cash)
+  {
+    // todo: this would be only dutch auction contract
+  }
+
+  /////////////////
+  // Margin Math //
+  /////////////////
+
+  /**
+   * @notice Calculate the initial or maintenance margin of account.
+   *         A negative value means the account is X amount over the required margin.
+   * @param expiries Sorted array of option holdings.
+   * @param marginType Initial or maintenance margin.
+   * @return margin Amount by which account is over or under the required margin.
+   */
+  function _calcMargin(ExpiryHolding[] memory expiries, MarginType marginType) internal view returns (int margin) {
+    for (uint i; i < expiries.length; i++) {
+      margin += _calcExpiryValue(expiries[i], marginType);
+    }
+
+    margin += _calcCashValue(marginType);
+  }
+
+  // Option Margin Math
+
+  /**
+   * @notice Calculate the discounted value of option holdings in a specific expiry.
+   * @param expiry All option holdings within an expiry.
+   * @param marginType Initial or maintenance margin.
+   * @return expiryValue Value of assets or debt of options in a given expiry.
+   */
+  function _calcExpiryValue(ExpiryHolding memory expiry, MarginType marginType) internal view returns (int expiryValue) {
+    expiryValue;
+    for (uint i; i < expiry.strikes.length; i++) {
+      expiryValue += _calcStrikeValue(expiry.strikes[i], marginType);
+    }
+  }
+
+  /**
+   * @notice Calculate the discounted value of option holdings in a specific strike.
+   * @param strikeHoldings All option holdings of the same strike.
+   * @param marginType Initial or maintenance margin.
+   * @return strikeValue Value of assets or debt of options of a given strike.
+   */
+  function _calcStrikeValue(StrikeHolding memory strikeHoldings, MarginType marginType)
+    internal
+    view
+    returns (int strikeValue)
+  {
+    // todo: get call, put, forward values
+  }
+
+  // Cash Margin Math
+
+  /**
+   * @notice Calculate the discounted value of cash in account.
+   * @param marginType Initial or maintenance margin.
+   * @return cashValue Discounted value of cash held in account.
+   */
+  function _calcCashValue(MarginType marginType) internal view returns (int cashValue) {
+    // todo: apply interest rate shock
+  }
+
+  //////////
+  // Util //
+  //////////
+
+  /**
+   * @notice Sort all option holdings into an array of
+   *         [expiries][strikes][calls / puts / forwards].
+   * @param assets Array of balances for given asset and subId.
+   * @return expiryHoldings Sorted array of option holdings.
+   */
+  function _sortHoldings(AccountStructs.AssetBalance[] memory assets)
+    internal
+    view
+    returns (ExpiryHolding[] memory expiryHoldings)
+  {
+    // todo: sort out each expiry / strike
+    // todo: ignore the lendingAsset
+    // todo: add limit to # of expiries and # of options
+  }
+
+  //////////
+  // View //
+  //////////
+
+  /**
+   * @notice Sort all option holdings of an account into an array of
+   *         [expiries][strikes][calls / puts / forwards].
+   * @param accountId ID of account to sort.
+   * @return expiryHoldings Sorted array of option holdings.
+   */
+  function getSortedHoldings(uint accountId) external view returns (ExpiryHolding[] memory expiryHoldings) {}
+
+  /**
+   * @notice Calculate the initial margin of account.
+   *         A negative value means the account is X amount over the required margin.
+   * @param expiries Sorted array of option holdings.
+   * @return margin Amount by which account is over or under the required margin.
+   */
+  // todo: public view function to get margin values directly through accountId
+  function getInitialMargin(ExpiryHolding[] memory expiries) external view returns (int margin) {
+    return _calcMargin(expiries, MarginType.INITIAL);
+  }
+
+  /**
+   * @notice Calculate the maintenance margin of account.
+   *         A negative value means the account is X amount over the required margin.
+   * @param expiries Sorted array of option holdings.
+   * @return margin Amount by which account is over or under the required margin.
+   */
+  function getMaintenanceMargin(ExpiryHolding[] memory expiries) external view returns (int margin) {
+    return _calcMargin(expiries, MarginType.MAINTENANCE);
+  }
+
+  ////////////
+  // Errors //
+  ////////////
+
+  error PCRM_OnlyAuction(address sender, address auction);
+}
