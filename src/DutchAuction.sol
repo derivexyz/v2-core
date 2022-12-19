b09a333e8d116ce82e854c370918d9ed36aa3c45
Beefmaster <cameron.j.baker1000@gmail.com>
2022-12-19 15:09:43 +1100

    added natspec

--- src/DutchAuction.sol
+++ src/DutchAuction.sol
@@ -26,13 +26,19 @@ contract DutchAuction is IDutchAuction {
     return parameters;
   }
 
-  // adds a risk manager that can initiate auctions
+  /// @notice Adds a risk manager that can initiate auctions
+  /// @dev This function is used to add a risk manager that can initiate auctions
+  /// @return bool returns true if the risk manager is added
+  /// @inheritdoc	Copies all missing tags from the base function (must be followed by the contract name)
   function addRiskManger() external returns(bool) {
     isRiskManagers[msg.sender] = true;
-    return true;
+    return isRiskManagers[msg.sender];
   }
   
-  // can only be called by the manager and will initiate an auction
+  /// @notice Called by the riskManager to start an auction
+  /// @dev Can only be auctioned by a risk manager and will start an auction
+  /// @param accountId The id of the account being liquidated
+  /// @return bytes32 the id of the auction that was just started
   function startAuction(uint accountId) external returns(bytes32) {
     if (!isRiskManagers[msg.sender]) {
       revert NotRiskManager(msg.sender);
@@ -40,8 +46,8 @@ contract DutchAuction is IDutchAuction {
 
     bytes32 auctionId = keccak256(abi.encodePacked(accountId, block.timestamp));
     uint price = spotFeed.getSpot(1);
-    int upperBound = getVMax(accountId, int(price));
-    int lowerBound = getVmin(accountId, int(price));
+    (int upperBound) = getVMax(accountId, int(price));
+    (int lowerBound) = getVmin(accountId, int(price));
 
     auctions[auctionId] = Auction({
       insolvent: false,
@@ -75,21 +81,31 @@ contract DutchAuction is IDutchAuction {
 
   }
 
+  /// @notice returns the details of an ongoing auction
+  /// @param auctionId the id of the auction that is being queried
+  /// @return Auction returns the struct of the auction details
   function auctionDetails(bytes32 auctionId) external view returns(Auction memory) {
     return auctions[auctionId];
   }
 
-  function currentAuctionPrice(uint auctionId) external view returns(uint) {}
-
   function endAuction(uint auctionId) external returns(uint) {}
 
-  function getMaxProportion(uint accountId) external returns(uint) {}
+  /// @notice Gets the maximum size of the portfolio that could be bought at the current price
+  /// @param accountId the id of the account being liquidated
+  /// @return uint the proportion of the portfolio that could be bought at the current price
+  function getMaxProportion(uint accountId) external returns(uint) {
+
+  }
 
 
   ///////////////
   // internal //
   ///////////////
 
+  /// @notice gets the upper bound for the liquidation price
+  /// @dev requires the accountId and the spot price to mark each asset at a particular value
+  /// @param accountId the accountId of the account that is being liquidated
+  /// @return spot the spot price of the asset, TODO: consider how this is going to work with options on different spot markets.
   function getVMax(uint accountId, int spot) internal returns(int) {
     (IPCRM.ExpiryHolding[] memory expiryHoldings, int cash) = IPCRM(msg.sender).getSortedHoldings(accountId);
     int portfolioMargin = cash;
@@ -104,25 +120,33 @@ contract DutchAuction is IDutchAuction {
     return portfolioMargin;
   } 
 
+  /// @notice gets the lower bound for the liquidation price
+  /// @dev requires the accountId and the spot price to mark each asset at a particular value
+  /// @param accountId the accountId of the account that is being liquidated
+  /// @return spot the spot price of the asset, TODO: consider how this is going to work with options on different spot markets.
   function getVmin(uint accountId, int spot) internal returns(int) {
 
     // TODO: need to do some more work on this. 
     // vmin is going to be difficult to compute
     (IPCRM.ExpiryHolding[] memory expiryHoldings, int cash) = IPCRM(msg.sender).getSortedHoldings(accountId);
     int portfolioMargin = cash;
-    for (uint i = 0; i < expiryHoldings.length; i++) {
+    // for (uint i = 0; i < expiryHoldings.length; i++) {
 
-      // iterate over all strike holdings, if they are Long calls mark them to spot, if they are long puts consider them at there strike, shorts to 0
+    //   // iterate over all strike holdings, if they are Long calls mark them to spot, if they are long puts consider them at there strike, shorts to 0
     
-      for (uint j = 0; j < expiryHoldings[i].strikes.length; j++) {
-        portfolioMargin += expiryHoldings[i].strikes[j].puts * int64(expiryHoldings[i].strikes[j].strike);
-        portfolioMargin += expiryHoldings[i].strikes[j].calls * spot;
-      }
-    }
+    //   for (uint j = 0; j < expiryHoldings[i].strikes.length; j++) {
+    //     portfolioMargin += expiryHoldings[i].strikes[j].puts * int64(expiryHoldings[i].strikes[j].strike);
+    //     portfolioMargin += expiryHoldings[i].strikes[j].calls * spot;
+    //   }
+    // }
 
     return portfolioMargin;
   }
 
+  /// @notice gets the current bid price for a particular auction at the current block
+  /// @dev returns the current bid price for a particular auction
+  /// @param auctionId the bytes32 id of an auctionId
+  /// @return int the current bid price for the auction
   function getCurrentBidPrice(bytes32 auctionId) view external returns(int) {
     // need to check if the auction is still ongoing
     // if not then return the lower bound
