3a7fdf21db30c83956f16db5d978d45857e770a0
Beefmaster <cameron.j.baker1000@gmail.com>
2022-12-20 16:39:57 +1100

    forge fmt

--- src/liquidation/DutchAuction.sol
+++ src/liquidation/DutchAuction.sol
@@ -6,7 +6,6 @@ import "../interfaces/IPCRM.sol";
 import "../interfaces/ISpotFeeds.sol";
 
 contract DutchAuction is IDutchAuction {
-  
   mapping(address => bool) public isRiskManagers;
   mapping(bytes32 => Auction) public auctions;
   DutchAuctionParameters public parameters;
@@ -15,13 +14,16 @@ contract DutchAuction is IDutchAuction {
   constructor(ISpotFeeds _spotFeed) {
     spotFeed = _spotFeed;
   }
-  
+
   /// @notice Sets the dutch Auction Parameters
   /// @dev This function is used to set the parameters for the dutch auction
   /// @param params A struct that contains all the parameters for the dutch auction
   /// @return Documents the parameters for the dutch auction that were just set.
   // TODO: needs to be rescrited to owner
-  function setDutchAuctionParameters(DutchAuctionParameters memory params) external returns(DutchAuctionParameters memory) {
+  function setDutchAuctionParameters(DutchAuctionParameters memory params)
+    external
+    returns (DutchAuctionParameters memory)
+  {
     // set the parameters for the dutch auction
     parameters = params;
     return parameters;
@@ -30,16 +32,16 @@ contract DutchAuction is IDutchAuction {
   /// @notice Adds a risk manager that can initiate auctions
   /// @dev This function is used to add a risk manager that can initiate auctions
   /// @return bool returns true if the risk manager is added
-  function addRiskManger() external returns(bool) {
+  function addRiskManger() external returns (bool) {
     isRiskManagers[msg.sender] = true;
     return isRiskManagers[msg.sender];
   }
-  
+
   /// @notice Called by the riskManager to start an auction
   /// @dev Can only be auctioned by a risk manager and will start an auction
   /// @param accountId The id of the account being liquidated
   /// @return bytes32 the id of the auction that was just started
-  function startAuction(uint accountId) external returns(bytes32) {
+  function startAuction(uint accountId) external returns (bytes32) {
     if (!isRiskManagers[msg.sender]) {
       revert NotRiskManager(msg.sender);
     }
@@ -55,11 +57,8 @@ contract DutchAuction is IDutchAuction {
       startBlock: block.number,
       endBlock: block.number + parameters.lengthOfAuction,
       dv: 0, // TODO: need to be able to calculate dv
-      auction: AuctionDetails({
-      accountId: accountId,
-      upperBound: upperBound,
-      lowerBound: lowerBound
-    })});
+      auction: AuctionDetails({accountId: accountId, upperBound: upperBound, lowerBound: lowerBound})
+    });
     return auctionId;
   }
 
@@ -67,32 +66,30 @@ contract DutchAuction is IDutchAuction {
   /// @dev Takes in the auction and returns the account id
   /// @param auctionId the bytesId that corresponds to a particular auction
   /// @return amount the amount as a percantage of the portfolio that the user is willing to purchase
-  function bid(bytes32 auctionId, int amount) external returns(uint) {
+  function bid(bytes32 auctionId, int amount) external returns (uint) {
     // need to check if the timelimit for the auction has been ecplised
     // the position is thus insolvent otherwise
     // need to check if this amount would put the portfolio over is matience marign
     // if so then revert
-    
+
     // send/ take money from the user if depending on the current priec
     // if the user has less margin then the amount they are bidding then get it from the security module
 
     // add bid
     // IPCRM.executeBid(accountId, msg.sender, amount, cashAmount); // not sure about the liquidator difference
-
   }
 
   /// @notice returns the details of an ongoing auction
   /// @param auctionId the id of the auction that is being queried
   /// @return Auction returns the struct of the auction details
-  function auctionDetails(bytes32 auctionId) external view returns(Auction memory) {
+  function auctionDetails(bytes32 auctionId) external view returns (Auction memory) {
     return auctions[auctionId];
   }
-
 
   /// @notice Gets the maximum size of the portfolio that could be bought at the current price
   /// @param accountId the id of the account being liquidated
   /// @return uint the proportion of the portfolio that could be bought at the current price
-  function getMaxProportion(uint accountId) external returns(uint) {}
+  function getMaxProportion(uint accountId) external returns (uint) {}
 
   ///////////////
   // internal //
@@ -102,7 +99,7 @@ contract DutchAuction is IDutchAuction {
   /// @dev requires the accountId and the spot price to mark each asset at a particular value
   /// @param accountId the accountId of the account that is being liquidated
   /// @return spot the spot price of the asset, TODO: consider how this is going to work with options on different spot markets.
-  function getVMax(uint accountId, int spot) internal returns(int) {
+  function getVMax(uint accountId, int spot) internal returns (int) {
     // (IPCRM.ExpiryHolding[] memory expiryHoldings, int cash) = IPCRM(msg.sender).getSortedHoldings(accountId);
     // int portfolioMargin = cash;
     // for (uint i = 0; i < expiryHoldings.length; i++) {
@@ -115,22 +112,21 @@ contract DutchAuction is IDutchAuction {
     // // need to discuss with mech how this is going to work
     // return portfolioMargin;
     return 0;
-  } 
+  }
 
   /// @notice gets the lower bound for the liquidation price
   /// @dev requires the accountId and the spot price to mark each asset at a particular value
   /// @param accountId the accountId of the account that is being liquidated
   /// @return spot the spot price of the asset, TODO: consider how this is going to work with options on different spot markets.
-  function getVmin(uint accountId, int spot) internal returns(int) {
-
-    // TODO: need to do some more work on this. 
+  function getVmin(uint accountId, int spot) internal returns (int) {
+    // TODO: need to do some more work on this.
     // vmin is going to be difficult to compute
     // (IPCRM.ExpiryHolding[] memory expiryHoldings, int cash) = IPCRM(msg.sender).getSortedHoldings(accountId);
     // int portfolioMargin = cash;
     // for (uint i = 0; i < expiryHoldings.length; i++) {
 
     //   // iterate over all strike holdings, if they are Long calls mark them to spot, if they are long puts consider them at there strike, shorts to 0
-    
+
     //   for (uint j = 0; j < expiryHoldings[i].strikes.length; j++) {
     //     portfolioMargin += expiryHoldings[i].strikes[j].puts * int64(expiryHoldings[i].strikes[j].strike);
     //     portfolioMargin += expiryHoldings[i].strikes[j].calls * spot;
@@ -145,13 +141,13 @@ contract DutchAuction is IDutchAuction {
   /// @dev returns the current bid price for a particular auction
   /// @param auctionId the bytes32 id of an auctionId
   /// @return int the current bid price for the auction
-  function getCurrentBidPrice(bytes32 auctionId) view external returns(int) {
+  function getCurrentBidPrice(bytes32 auctionId) external view returns (int) {
     // need to check if the auction is still ongoing
     // if not then return the lower bound
-    // otherwise return using dv 
+    // otherwise return using dv
     Auction memory auction = auctions[auctionId];
     int upperBound = auction.auction.upperBound;
-    uint numSteps = block.number / parameters.stepInterval; // will round down to whole number. 
+    uint numSteps = block.number / parameters.stepInterval; // will round down to whole number.
 
     // dv = (Vmax - Vmin) * numSteps
     return upperBound - int(auction.dv * numSteps);
@@ -163,16 +159,15 @@ contract DutchAuction is IDutchAuction {
 
   // emmited when an auction starts
   event AuctionStarted(bytes32 auctionId, uint accountId, uint upperBound, uint lowerBound);
-  
+
   // emmited when a bid is placed
   event Bid(bytes32 auctionId, address bidder, uint amount);
-  
+
   // emmited when an auction results in insolvency
   event Insolvent(bytes32 auctionId, uint accountId);
-  
-  // emmited when an auction ends, either by insolvency or by the assets of an account being purchased. 
-  event AuctionEnded(bytes32 auctionId, uint accountId, uint amount);
 
+  // emmited when an auction ends, either by insolvency or by the assets of an account being purchased.
+  event AuctionEnded(bytes32 auctionId, uint accountId, uint amount);
 
   ////////////
   // ERRORS //
