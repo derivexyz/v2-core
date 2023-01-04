a76d7e119798a5cdd6f3ebbba5a84eca5245fd0b
Cameron <cameron.j.baker1000@gmail.com>
2023-01-04 11:15:44 +1100

    feat (Auction): preliminary interface (#56)
    ## Summary
    
    Adding the contract for the dutch auction functionality
    
    ## Details
    
    The dutch auction contract is a contract that is external to the
    liquidation contract and will facilitate the atomic auctioning of
    assets. The risk manager will directly interface with this contract as a
    trusted owner.
    
    ## Todo
    
    - [x] Draft interface for the dutch auction contract
    - [x] Interface for the Dutch auction contract
    - [x] Basic functions for Dutch auction
    - [x] Unit tests for the dutch auction contract view functions, etc
    - [x] Drat interface for Security module with basic functionality to
    handle insolvency testing
    
    ## Checklist
    
    Ensure you completed **all of the steps** below before submitting your
    pull request:
    
    - [x] Add
    [natspec](https://docs.soliditylang.org/en/latest/natspec-format.html)
    for all functions / parameters
    - [x] Ran `forge snapshot`
    - [x] Ran `forge fmt`
    - [x] Ran `forge test`
    - [x] [Triage Slither issues](../README.md#triage-issues), and post
    uncertain ones in the PR
    - [x] 100% test coverage on code changes
    
    ### Slither Issues (Optional)
    
    If you're unsure about a new issue reported by Slither, copy them here
    so others can verify as well.
    
    Co-authored-by: Anton Cheng <antonassocareer@gmail.com>
    Co-authored-by: Joshua Kim <joshua0702@gmail.com>

---
+++ src/liquidation/DutchAuction.sol
@@ -0,0 +1,247 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+// interfaces
+import "../interfaces/IPCRM.sol";
+import "../interfaces/IDutchAuction.sol";
+
+// inherited
+import "synthetix/Owned.sol";
+
+/**
+ * @title Dutch Auction
+ * @author Lyra
+ * @notice Is used to liquidate an account that does not meet the margin requirements
+ * 1. The auction is started by the risk Manager
+ * 2. Bids are taken in a descending fashion until the matinance margin
+ * 3. A scalar is applied to the assets of the portfolio and are transfered to the bidder
+ * 4. This continues until matienance margin is met or until the portofolio is declared as insolvent
+ * where the security module will step into to handle the risk
+ * @dev This contract has a 1 to 1 relationship with a particular risk manager.
+ */
+contract DutchAuction is IDutchAuction, Owned {
+  struct AuctionDetails {
+    /// the accountId that is being liquidated
+    uint accountId;
+    /// The upperBound(starting price) of the auction in cash asset
+    int upperBound;
+    /// The lowerBound(ending price) of the auction in cash asset
+    int lowerBound;
+  }
+
+  struct Auction {
+    /// struct that references the auction details
+    AuctionDetails auction;
+    /// Boolean that will be switched when the auction price passes through 0
+    bool insolvent;
+    /// If an auction is active
+    bool ongoing;
+    /// The startTime of the auction
+    uint startTime;
+    /// The endTime of the auction
+    uint endTime;
+    /// The change in value of the portfolio per step in dollars
+    uint dv;
+  }
+
+  struct DutchAuctionParameters {
+    /// Length of each step in seconds
+    uint stepInterval;
+    /// Total length of an auction in seconds
+    uint lengthOfAuction;
+    /// The address of the security module
+    address securityModule;
+  }
+
+  /// @dev auctionId => Auction for when an auction is started
+  mapping(bytes32 => Auction) public auctions;
+
+  /// @dev auctionId => auctionOwner :TODO: for the risk manager that started the auction??
+  mapping(bytes32 => address) public auctionOwner;
+
+  /// @dev The risk manager that is the parent of the dutch auction contract
+  IPCRM public immutable riskManager;
+
+  /// @dev The parameters for the dutch auction
+  DutchAuctionParameters public parameters;
+
+  ////////////////////////
+  //    Constructor     //
+  ////////////////////////
+
+  constructor(address _riskManager) Owned() {
+    riskManager = IPCRM(_riskManager);
+  }
+
+  /**
+   * @notice Sets the dutch Auction Parameters
+   * @dev This function is used to set the parameters for the dutch auction
+   * @param _parameters A struct that contains all the parameters for the dutch auction
+   * @return Documents the parameters for the dutch auction that were just set.
+   */
+  function setDutchAuctionParameters(DutchAuctionParameters memory _parameters)
+    external
+    onlyOwner
+    returns (DutchAuctionParameters memory)
+  {
+    // set the parameters for the dutch auction
+    parameters = _parameters;
+    return parameters;
+  }
+
+  /**
+   * @notice Called by the riskManager to start an auction
+   * @dev Can only be auctioned by a risk manager and will start an auction
+   * @param accountId The id of the account being liquidated
+   * @return bytes32 the id of the auction that was just started
+   */
+  function startAuction(uint accountId) external returns (bytes32) {
+    if (address(riskManager) != msg.sender) {
+      revert DA_NotRiskManager();
+    }
+
+    //TODO: finish this function
+
+    bytes32 auctionId = keccak256(abi.encodePacked(accountId, block.timestamp));
+
+    // TODO: risk manager prelimary function will be more fleshed out later.
+    uint spot = riskManager.getSpot();
+
+    (int upperBound) = _getVMax(accountId, int(spot));
+    (int lowerBound) = _getVMin(accountId, int(spot));
+
+    auctions[auctionId] = Auction({
+      insolvent: false,
+      ongoing: true,
+      startTime: block.timestamp,
+      endTime: block.timestamp + parameters.lengthOfAuction,
+      dv: 0, // TODO: need to be able to calculate dv
+      auction: AuctionDetails({accountId: accountId, upperBound: upperBound, lowerBound: lowerBound})
+    });
+    return auctionId;
+  }
+
+  /**
+   * @notice a user submits a bid for a particular auction
+   * @dev Takes in the auction and returns the account id
+   * @param auctionId the bytesId that corresponds to a particular auction
+   * @return amount the amount as a percantage of the portfolio that the user is willing to purchase
+   */
+  function bid(bytes32 auctionId, int amount) external returns (uint) {
+    // need to check if the timelimit for the auction has been ecplised
+    // the position is thus insolvent otherwise
+    // need to check if this amount would put the portfolio over is matience marign
+    // if so then revert
+
+    // send/ take money from the user if depending on the current priec
+    // if the user has less margin then the amount they are bidding then get it from the security module
+
+    // add bid
+    // IPCRM.executeBid(accountId, msg.sender, amount, cashAmount); // not sure about the liquidator difference
+  }
+
+  /**
+   * @notice returns the details of an ongoing auction
+   * @param auctionId the id of the auction that is being queried
+   * @return Auction returns the struct of the auction details
+   */
+  function getAuctionDetails(bytes32 auctionId) external view returns (Auction memory) {
+    return auctions[auctionId];
+  }
+
+  /**
+   * @notice Gets the maximum size of the portfolio that could be bought at the current price
+   * @param accountId the id of the account being liquidated
+   * @return uint the proportion of the portfolio that could be bought at the current price
+   */
+  function getMaxProportion(uint accountId) external returns (uint) {}
+
+  /**
+   * @notice gets the upper bound for the liquidation price
+   * @dev requires the accountId and the spot price to mark each asset at a particular value
+   * @param accountId the accountId of the account that is being liquidated
+   * @param spot the spot price of the asset,
+   */
+  function getVMax(uint accountId, int spot) external view returns (int) {
+    return _getVMax(accountId, spot);
+  }
+
+  /**
+   * @notice gets the upper bound for the liquidation price
+   * @dev requires the accountId and the spot price to mark each asset at a particular value
+   * @param accountId the accountId of the account that is being liquidated
+   * @param spot the spot price of the asset,
+   */
+  function getVMin(uint accountId, int spot) external view returns (int) {
+    return _getVMin(accountId, spot);
+  }
+
+  ///////////////
+  // internal //
+  ///////////////
+
+  /**
+   * @notice gets the upper bound for the liquidation price
+   * @dev requires the accountId and the spot price to mark each asset at a particular value
+   * @param accountId the accountId of the account that is being liquidated
+   * @param spot the spot price of the asset,
+   * TODO: consider how this is going to work with options on different spot markets.
+   */
+  function _getVMax(uint accountId, int spot) internal view returns (int) {
+    // (IPCRM.ExpiryHolding[] memory expiryHoldings, int cash) = IPCRM(msg.sender).getSortedHoldings(accountId);
+    int portfolioMargin = 0; // = cash
+    // for (uint i = 0; i < expiryHoldings.length; i++) {
+    //   // iterate over all strike holdings, if they are Long calls mark them to spot, if they are long puts consider them at there strike, shorts to 0
+    //   for (uint j = 0; j < expiryHoldings[i].strikes.length; j++) {
+    //     portfolioMargin += expiryHoldings[i].strikes[j].puts * int64(expiryHoldings[i].strikes[j].strike);
+    //     portfolioMargin += expiryHoldings[i].strikes[j].calls * spot;
+    //   }
+    // }
+    // // need to discuss with mech how this is going to work
+    // return portfolioMargin;
+    return portfolioMargin;
+  }
+
+  /**
+   * @notice gets the lower bound for the liquidation price
+   * @dev requires the accountId and the spot price to mark each asset at a particular value
+   * @param accountId the accountId of the account that is being liquidated
+   * @param spot the spot price of the asset
+   *  TODO: consider how this is going to work with options on different spot markets.
+   */
+  function _getVMin(uint accountId, int spot) internal pure returns (int) {
+    // TODO: need to do some more work on this.
+    // vmin is going to be difficult to compute
+    // (IPCRM.ExpiryHolding[] memory expiryHoldings, int cash) = IPCRM(msg.sender).getSortedHoldings(accountId);
+    int portfolioMargin = 0; // = cash;
+    // for (uint i = 0; i < expiryHoldings.length; i++) {
+
+    //   // iterate over all strike holdings, if they are Long calls mark them to spot, if they are long puts consider them at there strike, shorts to 0
+
+    //   for (uint j = 0; j < expiryHoldings[i].strikes.length; j++) {
+    //     portfolioMargin += expiryHoldings[i].strikes[j].puts * int64(expiryHoldings[i].strikes[j].strike);
+    //     portfolioMargin += expiryHoldings[i].strikes[j].calls * spot;
+    //   }
+    // }
+
+    return portfolioMargin;
+  }
+
+  /**
+   * @notice gets the current bid price for a particular auction at the current block
+   * @dev returns the current bid price for a particular auction
+   * @param auctionId the bytes32 id of an auctionId
+   * @return int the current bid price for the auction
+   */
+  function getCurrentBidPrice(bytes32 auctionId) external view returns (int) {
+    // need to check if the auction is still ongoing
+    // if not then return the lower bound
+    // otherwise return using dv
+    Auction memory auction = auctions[auctionId];
+    int upperBound = auction.auction.upperBound;
+    uint numSteps = block.timestamp / parameters.stepInterval; // will round down to whole number.
+
+    // dv = (Vmax - Vmin) * numSteps
+    return upperBound - int(auction.dv * numSteps);
+  }
+}
---
+++ test/auction/unit-tests/TestStartAuction.t.sol
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import "../../../src/liquidation/DutchAuction.sol";
+import "../../../src/Account.sol";
+import "../../shared/mocks/MockERC20.sol";
+import "../../shared/mocks/MockAsset.sol";
+
+import "../../../src/liquidation/DutchAuction.sol";
+
+import "../../shared/mocks/MockManager.sol";
+import "../../shared/mocks/MockFeed.sol";
+
+contract UNIT_TestStartAuction is Test {
+  address alice;
+  address bob;
+  uint aliceAcc;
+  uint bobAcc;
+  Account account;
+  MockERC20 usdc;
+  MockAsset usdcAsset;
+  MockManager manager;
+  DutchAuction dutchAuction;
+  DutchAuction.DutchAuctionParameters public dutchAuctionParameters;
+
+  uint tokenSubId = 1000;
+
+  function setUp() public {
+    deployMockSystem();
+    setupAccounts();
+  }
+
+  function setupAccounts() public {
+    alice = address(0xaa);
+    bob = address(0xbb);
+    usdc.approve(address(usdcAsset), type(uint).max);
+    // usdcAsset.deposit(ownAcc, 0, 100_000_000e18);
+    aliceAcc = account.createAccount(alice, manager);
+    bobAcc = account.createAccount(bob, manager);
+  }
+
+  /// @dev deploy mock system
+  function deployMockSystem() public {
+    /* Base Layer */
+    account = new Account("Lyra Margin Accounts", "LyraMarginNFTs");
+
+    /* Wrappers */
+    usdc = new MockERC20("usdc", "USDC");
+
+    // usdc asset: deposit with usdc, cannot be negative
+    usdcAsset = new MockAsset(IERC20(usdc), IAccount(address(account)), false);
+    usdcAsset = new MockAsset(IERC20(usdc), IAccount(address(account)), false);
+
+    /* Risk Manager */
+    manager = new MockManager(address(account));
+
+    dutchAuction = new DutchAuction(address(manager));
+
+    dutchAuction.setDutchAuctionParameters(
+      DutchAuction.DutchAuctionParameters({stepInterval: 2, lengthOfAuction: 200, securityModule: address(1)})
+    );
+  }
+
+  function mintAndDeposit(
+    address user,
+    uint accountId,
+    MockERC20 token,
+    MockAsset assetWrapper,
+    uint subId,
+    uint amount
+  ) public {
+    token.mint(user, amount);
+
+    vm.startPrank(user);
+    token.approve(address(assetWrapper), type(uint).max);
+    assetWrapper.deposit(accountId, subId, amount);
+    vm.stopPrank();
+  }
+
+  ///////////
+  // TESTS //
+  ///////////
+
+  /////////////////////////
+  // Start Auction Tests //
+  /////////////////////////
+
+  function testStartAuction() public {
+    // making call from Riskmanager of the dutch auction contract
+    vm.startPrank(address(manager));
+
+    // start an auction on Alice's account
+    bytes32 auctionId = dutchAuction.startAuction(aliceAcc);
+    assertEq(auctionId, keccak256(abi.encodePacked(aliceAcc, block.timestamp)));
+
+    // testing that the view returns the correct auction.
+    DutchAuction.Auction memory auction = dutchAuction.getAuctionDetails(auctionId);
+    assertEq(auction.auction.accountId, aliceAcc);
+
+    // getting the current bid price
+    int currentBidPrice = dutchAuction.getCurrentBidPrice(auctionId);
+    assertEq(currentBidPrice, 0);
+  }
+
+  function testCannotStartWithNonManager() public {
+    vm.startPrank(address(0xdead));
+
+    // start an auction on Alice's account
+    vm.expectRevert(IDutchAuction.DA_NotRiskManager.selector);
+    dutchAuction.startAuction(aliceAcc);
+  }
+}
