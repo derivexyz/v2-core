4037123cc055a93cc867c202741d8ddbefc704d9
Joshua Kim <joshua0702@gmail.com>
2023-01-06 13:52:03 -0800

    feat: add critical params

--- src/risk-managers/SpotJumpOracle.sol
+++ src/risk-managers/SpotJumpOracle.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "src/interfaces/ISpotFeeds.sol";
 
 /**
  * @title SpotJumpOracle
@@ -11,10 +12,29 @@ pragma solidity ^0.8.13;
 contract SpotJumpOracle {
   ISpotFeeds public spotFeeds;
   uint public feedId;
-  constructor(address _spotFeeds, uint _feedId) {
-    spotFeeds = ISpotFeeds(_spotFeeds);
-    feedId = _feedId;
+
+  uint32[20] jumps;
+  JumpParams public params;
+
+  struct JumpParams {
+    // 500 bps would imply the first bucket is 5% -> 5% + width 
+    uint32 start;
+    // 150 bps would imply [0-1.5%, 1.5-3.0%, ...]
+    uint32 width;
+    // sec until jump is discarded
+    uint32 duration;
+    // sec until value is considered stale
+    uint32 staleLimit;
+    // sec to reference spot used to calculate jump
+    uint32 secToReference;
   }
 
-  
+  constructor(address _spotFeeds, uint _feedId, JumpParams memory _params, uint32[20] memory _initialJumps) {
+    spotFeeds = ISpotFeeds(_spotFeeds);
+    feedId = _feedId;
+    params = _params;
+    jumps = _initialJumps;
+  }
+
+
 }
