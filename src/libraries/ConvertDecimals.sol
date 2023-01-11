f66243a7cd392667204f2b1b81a7bf217070eb25
Anton Cheng <antonassocareer@gmail.com>
2023-01-11 10:29:04 +0700

    chore: remove mulDiv in convertDecimals

--- src/libraries/ConvertDecimals.sol
+++ src/libraries/ConvertDecimals.sol
@@ -71,26 +71,4 @@ library ConvertDecimals {
     // scale up
     return amount * (10 ** (toDecimals - 18));
   }
-
-  /**
-   * @dev A unit factor is divided out after the product of x and y is evaluated,
-   * so that product must be less than 2**256.
-   * @return The result of multiplying x and y, interpreting the operands as fixed-point
-   * decimals.
-   */
-  function multiplyDecimal(uint x, uint y) internal pure returns (uint) {
-    /* Divide by UNIT to remove the extra factor introduced by the product. */
-    return (x * y) / UNIT;
-  }
-
-  /**
-   * @dev y is divided after the product of x and the standard precision unit
-   * is evaluated, so the product of x and UNIT must be less than 2**256.
-   * @return The result of safely dividing x and y. The return value is a high
-   * precision decimal.
-   */
-  function divideDecimal(uint x, uint y) internal pure returns (uint) {
-    /* Reintroduce the UNIT factor that will be divided out by y. */
-    return (x * UNIT) / y;
-  }
 }
