af25ff102b3e37ce105ade4572d2d25cc0aa8f56
Lyndon Chang <lyndnc99@gmail.com>
2023-01-23 15:07:52 +1100

    chore: ICashAsset inherit IAsset

--- src/assets/CashAsset.sol
+++ src/assets/CashAsset.sol
@@ -7,7 +7,6 @@ import "openzeppelin/utils/math/SafeCast.sol";
 import "synthetix/Owned.sol";
 import "synthetix/SignedDecimalMath.sol";
 import "synthetix/DecimalMath.sol";
-import "../interfaces/IAsset.sol";
 import "../interfaces/IAccounts.sol";
 import "../interfaces/ICashAsset.sol";
 import "../interfaces/IInterestRateModel.sol";
@@ -20,7 +19,7 @@ import "../libraries/ConvertDecimals.sol";
  * @author Lyra
  */
 
-contract CashAsset is ICashAsset, Owned, IAsset {
+contract CashAsset is ICashAsset, Owned {
   using SafeERC20 for IERC20Metadata;
   using ConvertDecimals for uint;
   using SafeCast for uint;
@@ -159,7 +158,7 @@ contract CashAsset is ICashAsset, Owned, IAsset {
     accounts.assetAdjustment(
       AccountStructs.AssetAdjustment({
         acc: recipientAccount,
-        asset: IAsset(address(this)),
+        asset: ICashAsset(address(this)),
         subId: 0,
         amount: int(amountInAccount),
         assetData: bytes32(0)
@@ -199,7 +198,7 @@ contract CashAsset is ICashAsset, Owned, IAsset {
     accounts.assetAdjustment(
       AccountStructs.AssetAdjustment({
         acc: accountId,
-        asset: IAsset(address(this)),
+        asset: ICashAsset(address(this)),
         subId: 0,
         amount: -int(cashAmount),
         assetData: bytes32(0)
@@ -235,7 +234,7 @@ contract CashAsset is ICashAsset, Owned, IAsset {
    */
   function calculateBalanceWithInterest(uint accountId) external returns (int balance) {
     _accrueInterest();
-    return _calculateBalanceWithInterest(accounts.getBalance(accountId, IAsset(address(this)), 0), accountId);
+    return _calculateBalanceWithInterest(accounts.getBalance(accountId, ICashAsset(address(this)), 0), accountId);
   }
 
   /// @notice Allows anyone to transfer accrued SM fees to the SM
@@ -246,7 +245,7 @@ contract CashAsset is ICashAsset, Owned, IAsset {
     accounts.assetAdjustment(
       AccountStructs.AssetAdjustment({
         acc: smId,
-        asset: IAsset(address(this)),
+        asset: ICashAsset(address(this)),
         subId: 0,
         amount: amountToSend,
         assetData: bytes32(0)
@@ -338,7 +337,7 @@ contract CashAsset is ICashAsset, Owned, IAsset {
     accounts.assetAdjustment(
       AccountStructs.AssetAdjustment({
         acc: accountToReceive,
-        asset: IAsset(address(this)),
+        asset: ICashAsset(address(this)),
         subId: 0,
         amount: lossAmountInCash.toInt256(),
         assetData: bytes32(0)
@@ -385,8 +384,8 @@ contract CashAsset is ICashAsset, Owned, IAsset {
    */
   function _calculateBalanceWithInterest(int preBalance, uint accountId) internal view returns (int interestBalance) {
     uint accountIndex = accountIdIndex[accountId];
-    if (accountIndex == 0) { return preBalance; }
-    
+    if (accountIndex == 0) return preBalance;
+
     uint indexChange;
     if (preBalance < 0) {
       indexChange = borrowIndex.divideDecimal(accountIndex);
@@ -467,7 +466,7 @@ contract CashAsset is ICashAsset, Owned, IAsset {
    * @dev get current account cash balance
    */
   // function _getStaleBalance(uint accountId) internal view returns (int balance) {
-  //   balance = accounts.getBalance(accountId, IAsset(address(this)), 0);
+  //   balance = accounts.getBalance(accountId, ICashAsset(address(this)), 0);
   // }
 
   ///////////////////
