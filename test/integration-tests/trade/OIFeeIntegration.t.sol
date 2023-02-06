416249dd251c2c55d1ee95ba418609a335ab24a5
Anton Cheng <antonassocareer@gmail.com>
2023-02-06 09:18:16 +0700

    Integration Test: Charging OI Fee (#139)
    ## Summary
    
    Simple Integration test on charging of OI fee.
    
    ## Details
    * Add few more helper function in `IntegrationTestBase`:
      * getCashBalacne
      * getOptionBalance
      * _submitTrade
    * Gas cost: 477,890 for 1 trade: 2 margin checks, and 2 fee transfers,
    (not including routing)
    
    ## Checklist
    
    Ensure you completed **all of the steps** below before submitting your
    pull request:
    
    - [x] Add
    [natspec](https://docs.soliditylang.org/en/latest/natspec-format.html)
    for all functions / parameters
    - [x] Ran `forge snapshot`
    - [x] Ran `forge fmt`
    - [x] Ran `forge test`
    - [x] [Triage Slither issues](../README.md#triage-issues), and post
    uncertain ones in the PR
    - [x] 100% test coverage on code changes

--- test/integration-tests/trade/OIFeeIntegration.t.sol
+++ test/integration-tests/trade/OIFeeIntegration.t.sol
@@ -5,21 +5,81 @@ import "forge-std/Test.sol";
 
 import "../shared/IntegrationTestBase.sol";
 import "src/interfaces/IManager.sol";
+import "src/libraries/OptionEncoding.sol";
 
 /**
  * @dev testing charge of OI fee in a real setting
  */
 contract INTEGRATION_OIFeeTest is IntegrationTestBase {
   address alice = address(0xaa);
-  uint accAcc;
+  uint aliceAcc;
+
+  address bob = address(0xbb);
+  uint bobAcc;
+
+  uint constant initCash = 200e18;
 
   function setUp() public {
     _setupIntegrationTestComplete();
+
+    aliceAcc = accounts.createAccount(alice, pcrm);
+    bobAcc = accounts.createAccount(bob, pcrm);
+
+    // allow this contract to submit trades
+    vm.prank(alice);
+    accounts.setApprovalForAll(address(this), true);
+    vm.prank(bob);
+    accounts.setApprovalForAll(address(this), true);
+
+    // init setup for both accounts
+    _depositCash(alice, aliceAcc, initCash);
+    _depositCash(bob, bobAcc, initCash);
   }
 
-  function testOIFee() public {
-    // not real test, just need this to make sure setUp works
-    accAcc = accounts.createAccount(alice, IManager(pcrm));
-    _depositCash(address(alice), accAcc, 5000e18);
+  function testChargeOIFee() public {
+    uint spot = feed.getSpot(feedId);
+
+    uint expiry = block.timestamp + 7 days;
+    uint strike = spot + 1000e18;
+    uint96 callToTrade = OptionEncoding.toSubId(expiry, strike, true);
+    int amountOfContracts = 10e18;
+    int premium = 50e18;
+
+    // alice send option to bob, bob send premium to alice
+    _submitTrade(aliceAcc, option, callToTrade, amountOfContracts, bobAcc, cash, 0, premium);
+
+    uint expectedOIFeeEach = spot * uint(amountOfContracts) / 1e18 / 1000;
+
+    assertEq(getCashBalance(pcrmFeeAcc), int(expectedOIFeeEach) * 2);
+    assertEq(getCashBalance(aliceAcc), int(initCash) + premium - int(expectedOIFeeEach));
+    assertEq(getCashBalance(bobAcc), int(initCash) - premium - int(expectedOIFeeEach));
+  }
+
+  function testClosingChargeNoFee() public {
+    // same setup
+    uint spot = feed.getSpot(feedId);
+    uint expiry = block.timestamp + 7 days;
+    uint strike = spot + 1000e18;
+    uint96 callToTrade = OptionEncoding.toSubId(expiry, strike, true);
+    int amountOfContracts = 10e18;
+    int premium = 50e18;
+
+    // open positions first
+    _submitTrade(aliceAcc, option, callToTrade, amountOfContracts, bobAcc, cash, 0, premium);
+
+    // pre-states before closing trade
+    int aliceCashBefore = getCashBalance(aliceAcc);
+    int bobCashBefore = getCashBalance(bobAcc);
+    int totalFeeBefore = getCashBalance(pcrmFeeAcc);
+
+    // alice pays premium2 to close position short with bob
+    int premium2 = 100e18;
+    _submitTrade(aliceAcc, cash, 0, premium2, bobAcc, option, callToTrade, amountOfContracts);
+
+    assertEq(getCashBalance(aliceAcc), aliceCashBefore - premium2);
+    assertEq(getCashBalance(bobAcc), bobCashBefore + premium2);
+
+    // no more fee
+    assertEq(getCashBalance(pcrmFeeAcc), totalFeeBefore);
   }
 }
