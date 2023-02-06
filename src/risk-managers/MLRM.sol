9632f851a5df5201d43f96a524e67828f0cdbbba
Joshua Kim <joshua0702@gmail.com>
2023-02-06 13:01:04 -0800

    feat: arrange assets

--- src/risk-managers/MLRM.sol
+++ src/risk-managers/MLRM.sol
@@ -11,8 +11,12 @@ import "src/interfaces/IDutchAuction.sol";
 import "src/interfaces/ICashAsset.sol";
 
 import "src/assets/Option.sol";
+import "src/risk-managers/PCRM.sol";
+import "src/assets/Option.sol";
+
 
 import "src/libraries/OptionEncoding.sol";
+import "src/libraries/PCRMGrouping.sol";
 import "src/libraries/Owned.sol";
 import "src/libraries/SignedDecimalMath.sol";
 import "src/libraries/DecimalMath.sol";
@@ -22,10 +26,40 @@ import "src/libraries/DecimalMath.sol";
  * @notice Risk Manager that controls transfer and margin requirements
  */
 
-contract PCRM is IManager, Owned {
+contract MLRM is IManager, Owned {
   using SignedDecimalMath for int;
   using DecimalMath for uint;
   using SafeCast for uint;
+
+  ///////////////
+  // Variables //
+  ///////////////
+
+  /// @dev asset used in all settlements and denominates margin
+  IAccounts public immutable account;
+
+  /// @dev spotFeeds that determine staleness and return prices
+  ISpotFeeds public spotFeeds;
+
+  /// @dev asset used in all settlements and denominates margin
+  ICashAsset public immutable cashAsset;
+
+  /// @dev reserved option asset
+  Option public immutable option;
+
+  /// @dev max number of strikes per expiry allowed to be held in one account
+  uint public constant MAX_STRIKES = 64;
+
+  ////////////////////////
+  //    Constructor     //
+  ////////////////////////
+
+  constructor(address account_, address spotFeeds_, address cashAsset_, address option_) Owned() {
+    account = IAccounts(account_);
+    spotFeeds = ISpotFeeds(spotFeeds_);
+    cashAsset = ICashAsset(cashAsset_);
+    option = Option(option_);
+  }
 
   function handleAdjustment(uint accountId, address, AccountStructs.AssetDelta[] memory, bytes memory)
     public
@@ -39,5 +73,61 @@ contract PCRM is IManager, Owned {
     // todo [Josh]: nextManager whitelist check
   }
 
+  function _arrangePortfolio(AccountStructs.AssetBalance[] memory assets)
+    internal
+    view
+    returns (PCRM.Portfolio memory portfolio)
+  {
+    // note: differs from PCRM._arrangePortfolio since forwards aren't filtered
+    portfolio.strikes = new PCRM.Strike[](
+      MAX_STRIKES > assets.length ? assets.length : MAX_STRIKES
+    );
 
+    PCRM.Strike memory currentStrike;
+    AccountStructs.AssetBalance memory currentAsset;
+    uint strikeIndex;
+    for (uint i; i < assets.length; ++i) {
+      currentAsset = assets[i];
+      if (address(currentAsset.asset) == address(option)) {
+        // decode subId
+        (uint expiry, uint strikePrice, bool isCall) = OptionEncoding.fromSubId(SafeCast.toUint96(currentAsset.subId));
+
+        // assume expiry = 0 means this is the first strike.
+        if (portfolio.expiry == 0) {
+          portfolio.expiry = expiry;
+        }
+
+        if (portfolio.expiry != expiry) {
+          revert MLRM_SingleExpiryPerAccount();
+        }
+
+        (strikeIndex, portfolio.numStrikesHeld) =
+          PCRMGrouping.findOrAddStrike(portfolio.strikes, strikePrice, portfolio.numStrikesHeld);
+
+        // add call or put balance
+        currentStrike = portfolio.strikes[strikeIndex];
+        if (isCall) {
+          currentStrike.calls += currentAsset.balance;
+        } else {
+          currentStrike.puts += currentAsset.balance;
+        }
+
+      } else if (address(currentAsset.asset) == address(cashAsset)) {
+        if (currentAsset.balance >= 0) {
+          revert MLRM_OnlyPositiveCash();
+        }
+        portfolio.cash = currentAsset.balance;
+      } else {
+        revert MLRM_UnsupportedAsset(address(currentAsset.asset));
+      }
+    }
+  } 
+
+  ////////////
+  // Errors //
+  ////////////
+
+  error MLRM_SingleExpiryPerAccount();
+  error MLRM_OnlyPositiveCash();
+  error MLRM_UnsupportedAsset(address asset); // could be used in both PCRM/MLRM
 }
