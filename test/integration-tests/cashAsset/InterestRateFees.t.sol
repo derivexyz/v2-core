8fe75970d84f6d1e06b6148a962d35cc1192e3d2
Joshua Kim <joshua0702@gmail.com>
2023-02-09 12:22:46 -0800

    helper: json io

--- test/integration-tests/cashAsset/InterestRateFees.t.sol
+++ test/integration-tests/cashAsset/InterestRateFees.t.sol
@@ -4,18 +4,21 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "forge-std/console2.sol";
 
+import "test/shared/utils/JsonMechIO.sol";
+
 import "../shared/IntegrationTestBase.sol";
 
 /**
  * @dev Tests to verify correct fees paid and received by suppliers and borrowers
  */
-contract INTEGRATION_InterestRateFeesTest is IntegrationTestBase {
+contract MECH_InterestRateFeesTest is IntegrationTestBase {
   address alice = address(0xace);
   address bob = address(0xb0b);
   address charlie = address(0xca1e);
   uint aliceAcc;
   uint bobAcc;
   uint charlieAcc;
+  JsonMechIO jsonParser;
 
   function setUp() public {
     _setupIntegrationTestComplete();
@@ -28,8 +31,21 @@ contract INTEGRATION_InterestRateFeesTest is IntegrationTestBase {
 
     vm.prank(charlie);
     accounts.setApprovalForAll(address(this), true);
+
+    // example of loading simple Uint array
+    // to print the json example, run in terminal: `forge test --match-test testNoInterestPaidForNoBorrow -vvvv`
+    jsonParser = new JsonMechIO();
+    uint[] memory sampleInputs = jsonParser.loadUints(
+      "/test/integration-tests/cashAsset/InterestRateFees.json", 
+      ".expectedAccruedInterest" // not the "." before the key
+    );
+
+    for (uint i; i < sampleInputs.length; i++) {
+      console2.log("json", i, sampleInputs[i]);
+    }
   }
 
+  // to do single test, in terminal run: `forge test --match-test testNoInterestPaidForNoBorrow -vvvv`
   // todo: test there is no fees when no borrow
   function testNoInterestPaidForNoBorrow() public {}
 
---
+++ test/shared/utils/JsonMechIO.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/StdJson.sol";
+
+// for more info on json parsing:
+// https://book.getfoundry.sh/cheatcodes/parse-json#decoding-json-objects-into-solidity-structs
+contract JsonMechIO is Test {
+  using stdJson for string;
+
+  function loadUints(string memory dirRelativeToRoot, string memory key) public view returns (uint[] memory vals) {
+    string memory path = string.concat(vm.projectRoot(), dirRelativeToRoot);
+    string memory json = vm.readFile(path);
+
+    // if key value is "pivots", say ".pivots", for more info use: 
+    // forge uses `jq` syntax for parsing: https://stedolan.github.io/jq/manual/#Basicfilters
+    return json.readUintArray(key);
+  }
+}
