62b25da5221d3cf7eb755ede6cda2a6ca519d0f9
Lyndon Chang <lyndnc99@gmail.com>
2023-02-27 14:21:18 +1100

    Test: begin multiway integration

---
+++ test/integration-tests/assets/optionAsset/MultiwayTrade.t.sol
@@ -0,0 +1,161 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/console2.sol";
+import "../../shared/IntegrationTestBase.sol";
+import "src/interfaces/IManager.sol";
+
+/**
+ * @dev testing charge of OI fee in a real setting
+ */
+contract INTEGRATION_MultiwayTradeTest is IntegrationTestBase {
+  using DecimalMath for uint;
+
+  address charlie = address(0xcc);
+  uint charlieAcc;
+  address dave = address(0xcc);
+  uint daveAcc;
+
+  int amountOfContracts = 1e18;
+
+  function setUp() public {
+    _setupIntegrationTestComplete();
+
+    charlieAcc = accounts.createAccount(charlie, pcrm);
+    daveAcc = accounts.createAccount(dave, pcrm);
+
+    // allow this contract to submit trades
+    vm.prank(charlie);
+    accounts.setApprovalForAll(address(this), true);
+    vm.prank(dave);
+    accounts.setApprovalForAll(address(this), true);
+
+    _depositCash(address(alice), aliceAcc, DEFAULT_DEPOSIT);
+    _depositCash(address(bob), bobAcc, DEFAULT_DEPOSIT);
+    _depositCash(address(charlie), charlieAcc, DEFAULT_DEPOSIT);
+    _depositCash(address(dave), daveAcc, DEFAULT_DEPOSIT);
+  }
+
+  function testCannotThreeWayTradeToYourself() public {
+    // ITM Call
+    uint callExpiry = block.timestamp + 4 weeks;
+    uint callStrike = 2000e18;
+    uint callId = option.getSubId(callExpiry, callStrike, true);
+
+    // Record pre balance
+    (int aliceBal, int bobBal, int charlieBal, int daveBal) = _getAllCashBalances();
+    console2.log("aliceBal  ", aliceBal);
+    console2.log("bobBal    ", bobBal);
+    console2.log("charlieBal", charlieBal);
+    console2.log("daveBal   ", daveBal);
+
+    AccountStructs.AssetTransfer[] memory transferBatch = new AccountStructs.AssetTransfer[](3);
+    // Alice transfer to Bob
+    transferBatch[0] = AccountStructs.AssetTransfer({
+      fromAcc: aliceAcc,
+      toAcc: bobAcc,
+      asset: option,
+      subId: callId,
+      amount: amountOfContracts,
+      assetData: bytes32(0)
+    });
+
+    // Bob transfers to Charlie
+    transferBatch[1] = AccountStructs.AssetTransfer({
+      fromAcc: bobAcc,
+      toAcc: charlieAcc,
+      asset: option,
+      subId: callId,
+      amount: amountOfContracts,
+      assetData: bytes32(0)
+    });
+
+    // Charlie transfer to Alice (closing the loop)
+    transferBatch[1] = AccountStructs.AssetTransfer({
+      fromAcc: charlieAcc,
+      toAcc: aliceAcc,
+      asset: option,
+      subId: callId,
+      amount: amountOfContracts,
+      assetData: bytes32(0)
+    });
+
+    vm.expectRevert(abi.encodeWithSelector(IAccounts.AC_CannotTransferAssetToOneself.selector, alice, aliceAcc));
+    accounts.submitTransfers(transferBatch, "");
+  }
+
+  function testThreeWayTradeITMCall() public {
+    // ITM Call
+    uint callExpiry = block.timestamp + 4 weeks;
+    uint callStrike = 2000e18;
+    uint callId = option.getSubId(callExpiry, callStrike, true);
+
+    // Record pre balance
+    (int aliceBal, int bobBal, int charlieBal, int daveBal) = _getAllCashBalances();
+    console2.log("aliceBal  ", aliceBal);
+    console2.log("bobBal    ", bobBal);
+    console2.log("charlieBal", charlieBal);
+    console2.log("daveBal   ", daveBal);
+
+    AccountStructs.AssetTransfer[] memory transferBatch = new AccountStructs.AssetTransfer[](3);
+    // Alice transfer to Bob
+    transferBatch[0] = AccountStructs.AssetTransfer({
+      fromAcc: aliceAcc,
+      toAcc: bobAcc,
+      asset: option,
+      subId: callId,
+      amount: amountOfContracts,
+      assetData: bytes32(0)
+    });
+
+    // Bob transfers to Charlie
+    transferBatch[1] = AccountStructs.AssetTransfer({
+      fromAcc: bobAcc,
+      toAcc: charlieAcc,
+      asset: option,
+      subId: callId,
+      amount: amountOfContracts,
+      assetData: bytes32(0)
+    });
+
+    // Charlie transfer to Alice (closing the loop)
+    transferBatch[1] = AccountStructs.AssetTransfer({
+      fromAcc: charlieAcc,
+      toAcc: aliceAcc,
+      asset: option,
+      subId: callId,
+      amount: amountOfContracts,
+      assetData: bytes32(0)
+    });
+
+    accounts.submitTransfers(transferBatch, "");
+
+    vm.warp(callExpiry);
+    _setSpotPriceAndSubmitForExpiry(ETH_PRICE + 1000e18, callExpiry);
+    (aliceBal, bobBal, charlieBal, daveBal) = _getAllCashBalances();
+    console2.log("aliceBal  ", aliceBal);
+    console2.log("bobBal    ", bobBal);
+    console2.log("charlieBal", charlieBal);
+    console2.log("daveBal   ", daveBal);
+
+    pcrm.settleAccount(aliceAcc);
+    pcrm.settleAccount(bobAcc);
+    pcrm.settleAccount(charlieAcc);
+    pcrm.settleAccount(daveAcc);
+
+    (aliceBal, bobBal, charlieBal, daveBal) = _getAllCashBalances();
+    console2.log("aliceBal  ", aliceBal);
+    console2.log("bobBal    ", bobBal);
+    console2.log("charlieBal", charlieBal);
+    console2.log("daveBal   ", daveBal);
+  }
+
+  function testFourWayTradeITMCall() public {}
+
+  function _getAllCashBalances() internal returns (int aliceBal, int bobBal, int charlieBal, int daveBal) {
+    aliceBal = getCashBalance(aliceAcc);
+    bobBal = getCashBalance(bobAcc);
+    charlieBal = getCashBalance(charlieAcc);
+    daveBal = getCashBalance(daveAcc);
+  }
+}
