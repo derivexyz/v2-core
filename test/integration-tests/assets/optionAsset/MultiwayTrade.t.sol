7e94e629eee4b97e75cabdf42b1d3d35da1f28be
LondonCalamari <lyndnc99@gmail.com>
2023-03-06 18:41:21 +1100

    Test: no oi fee charged test, 4 way trade

--- test/integration-tests/assets/optionAsset/MultiwayTrade.t.sol
+++ test/integration-tests/assets/optionAsset/MultiwayTrade.t.sol
@@ -36,7 +36,7 @@ contract INTEGRATION_MultiwayTradeTest is IntegrationTestBase {
     _depositCash(address(dave), daveAcc, DEFAULT_DEPOSIT);
   }
 
-  function testThreeWayTradeToYourself() public {
+  function testThreeWayTradeNoOIFee() public {
     uint callExpiry = block.timestamp + 4 weeks;
     uint callStrike = 2000e18;
     uint callId = option.getSubId(callExpiry, callStrike, true);
@@ -47,9 +47,9 @@ contract INTEGRATION_MultiwayTradeTest is IntegrationTestBase {
     console2.log("charlieBal", charlieBal);
     console2.log("daveBal   ", daveBal);
 
-    AccountStructs.AssetTransfer[] memory transferBatch = new AccountStructs.AssetTransfer[](2);
+    AccountStructs.AssetTransfer[] memory transferBatch = new AccountStructs.AssetTransfer[](3);
 
-    // // Alice transfer to Bob
+    // Alice transfer to Bob
     transferBatch[0] = AccountStructs.AssetTransfer({
       fromAcc: aliceAcc,
       toAcc: bobAcc,
@@ -69,33 +69,8 @@ contract INTEGRATION_MultiwayTradeTest is IntegrationTestBase {
       assetData: bytes32(0)
     });
 
-    //  // Charlie transfer to Alice (closing the loop)
-    //  transferBatch[2] = AccountStructs.AssetTransfer({
-    //   fromAcc: charlieAcc,
-    //   toAcc: aliceAcc,
-    //   asset: option,
-    //   subId: callId,
-    //   amount: amountOfContracts,
-    //   assetData: bytes32(0)
-    // });
-
-    accounts.submitTransfers(transferBatch, "");
-    (aliceBal, bobBal, charlieBal, daveBal) = _getAllCashBalances();
-    console2.log("aliceBal  ", aliceBal);
-    console2.log("bobBal    ", bobBal);
-    console2.log("charlieBal", charlieBal);
-    console2.log("daveBal   ", daveBal);
-
-    assertEq(uint(aliceBal), DEFAULT_DEPOSIT-2e18);
-    assertEq(uint(bobBal), DEFAULT_DEPOSIT);
-    assertEq(uint(charlieBal), DEFAULT_DEPOSIT-2e18);
-    assertEq(accounts.getBalance(aliceAcc, option, callId), -amountOfContracts);
-    assertEq(accounts.getBalance(bobAcc, option, callId), 0);
-    assertEq(accounts.getBalance(charlieAcc, option, callId), amountOfContracts);
-
-
-    // Charlie transfer to Alice (closing the loop)
-    AccountStructs.AssetTransfer memory finalTransfer = AccountStructs.AssetTransfer({
+     // Charlie transfer to Alice (closing the loop)
+     transferBatch[2] = AccountStructs.AssetTransfer({
       fromAcc: charlieAcc,
       toAcc: aliceAcc,
       asset: option,
@@ -103,22 +78,14 @@ contract INTEGRATION_MultiwayTradeTest is IntegrationTestBase {
       amount: amountOfContracts,
       assetData: bytes32(0)
     });
-    accounts.submitTransfer(finalTransfer, "");
 
+    // After all transfers no oi fee charged since option goes back to alice
+    accounts.submitTransfers(transferBatch, "");
     (aliceBal, bobBal, charlieBal, daveBal) = _getAllCashBalances();
-    console2.log("aliceBal  ", aliceBal);
-    console2.log("bobBal    ", bobBal);
-    console2.log("charlieBal", charlieBal);
-    console2.log("daveBal   ", daveBal);
 
-    // todo 3 way batch vs 2 and 1 transfer results in different OI fees
-    // assertEq(uint(aliceBal), DEFAULT_DEPOSIT);
+    assertEq(uint(aliceBal), DEFAULT_DEPOSIT);
     assertEq(uint(bobBal), DEFAULT_DEPOSIT);
-    // assertEq(uint(charlieBal), DEFAULT_DEPOSIT);
-
-    assertEq(accounts.getBalance(aliceAcc, option, callId), 0);
-    assertEq(accounts.getBalance(bobAcc, option, callId), 0);
-    assertEq(accounts.getBalance(charlieAcc, option, callId), 0);
+    assertEq(uint(charlieBal), DEFAULT_DEPOSIT);
   }
 
   function testThreeWayTradeITMCall() public {
@@ -168,7 +135,6 @@ contract INTEGRATION_MultiwayTradeTest is IntegrationTestBase {
     assertEq(uint(aliceBal), DEFAULT_DEPOSIT-uint(priceIncrease) - oiFee);
     assertEq(uint(bobBal), DEFAULT_DEPOSIT);
     assertEq(uint(charlieBal), DEFAULT_DEPOSIT+uint(priceIncrease) - oiFee);
-    assertEq(uint(bobBal), DEFAULT_DEPOSIT);
   }
 
   function testThreeWayTradeITMPut() public {
@@ -208,9 +174,7 @@ contract INTEGRATION_MultiwayTradeTest is IntegrationTestBase {
 
     vm.warp(putExpiry);
     int priceDecrease = 1000e18;
-    // console2.log("SPOT", feed.getSpot());
     _setSpotPriceAndSubmitForExpiry(ETH_PRICE - priceDecrease, putExpiry);
-    // console2.log("SPOT", feed.getSpot());
     pcrm.settleAccount(aliceAcc);
     pcrm.settleAccount(bobAcc);
     pcrm.settleAccount(charlieAcc);
@@ -221,10 +185,68 @@ contract INTEGRATION_MultiwayTradeTest is IntegrationTestBase {
     assertEq(uint(aliceBal), DEFAULT_DEPOSIT-uint(priceDecrease) - oiFee);
     assertEq(uint(bobBal), DEFAULT_DEPOSIT);
     assertEq(uint(charlieBal), DEFAULT_DEPOSIT+uint(priceDecrease) - oiFee);
-    assertEq(uint(bobBal), DEFAULT_DEPOSIT);
   }
 
-  function testFourWayTradeITMCall() public {}
+  function testFourWayTradeITMCall() public {
+    uint oiFee = (pcrm.OIFeeRateBPS()).multiplyDecimal(feed.getSpot());
+
+    // ATM Call
+    uint callExpiry = block.timestamp + 4 weeks;
+    uint callStrike = 2000e18;
+    uint callId = option.getSubId(callExpiry, callStrike, true);
+
+    // Record pre balance
+    (int aliceBal, int bobBal, int charlieBal, int daveBal) = _getAllCashBalances();
+
+    AccountStructs.AssetTransfer[] memory transferBatch = new AccountStructs.AssetTransfer[](3);
+
+    // Alice transfer to Bob
+    transferBatch[0] = AccountStructs.AssetTransfer({
+      fromAcc: aliceAcc,
+      toAcc: bobAcc,
+      asset: option,
+      subId: callId,
+      amount: amountOfContracts,
+      assetData: bytes32(0)
+    });
+
+    // Bob transfers to Charlie
+    transferBatch[1] = AccountStructs.AssetTransfer({
+      fromAcc: bobAcc,
+      toAcc: charlieAcc,
+      asset: option,
+      subId: callId,
+      amount: amountOfContracts,
+      assetData: bytes32(0)
+    });
+
+    // Charlie transfers to Dave
+    transferBatch[2] = AccountStructs.AssetTransfer({
+      fromAcc: charlieAcc,
+      toAcc: daveAcc,
+      asset: option,
+      subId: callId,
+      amount: amountOfContracts,
+      assetData: bytes32(0)
+    });
+
+    accounts.submitTransfers(transferBatch, "");
+
+    vm.warp(callExpiry);
+    int priceIncrease = 1000e18;
+    _setSpotPriceAndSubmitForExpiry(ETH_PRICE + priceIncrease, callExpiry);
+    pcrm.settleAccount(aliceAcc);
+    pcrm.settleAccount(bobAcc);
+    pcrm.settleAccount(charlieAcc);
+    pcrm.settleAccount(daveAcc);
+
+    // Alice's loss should be charlies gain
+    (aliceBal, bobBal, charlieBal, daveBal) = _getAllCashBalances();
+    assertEq(uint(aliceBal), DEFAULT_DEPOSIT-uint(priceIncrease) - oiFee);
+    assertEq(uint(bobBal), DEFAULT_DEPOSIT);
+    assertEq(uint(charlieBal), DEFAULT_DEPOSIT);
+    assertEq(uint(daveBal), DEFAULT_DEPOSIT+uint(priceIncrease) - oiFee);
+  }
 
   function _getAllCashBalances() internal returns (int aliceBal, int bobBal, int charlieBal, int daveBal) {
     aliceBal = getCashBalance(aliceAcc);
