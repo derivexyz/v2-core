e1313488e034d0a1179a461bb5aed21633dda463
LondonCalamari <lyndnc99@gmail.com>
2023-03-07 15:08:26 +1100

    Test: OTM trades

--- test/integration-tests/assets/optionAsset/MultiwayTrade.t.sol
+++ test/integration-tests/assets/optionAsset/MultiwayTrade.t.sol
@@ -187,6 +187,152 @@ contract INTEGRATION_MultiwayTradeTest is IntegrationTestBase {
     assertEq(uint(charlieBal), DEFAULT_DEPOSIT + uint(priceDecrease) - oiFee);
   }
 
+  function testThreeWayTradeOTMCall() public {
+    uint oiFee = (pcrm.OIFeeRateBPS()).multiplyDecimal(feed.getSpot());
+    int premium = 1000e18;
+
+    // ATM Call
+    uint callExpiry = block.timestamp + 4 weeks;
+    uint callStrike = 2000e18;
+    uint callId = option.getSubId(callExpiry, callStrike, true);
+
+    // Record pre balance
+    (int aliceBal, int bobBal, int charlieBal, int daveBal) = _getAllCashBalances();
+
+    AccountStructs.AssetTransfer[] memory transferBatch = new AccountStructs.AssetTransfer[](4);
+
+    // Alice transfer option to Bob for premium
+    transferBatch[0] = AccountStructs.AssetTransfer({
+      fromAcc: aliceAcc,
+      toAcc: bobAcc,
+      asset: option,
+      subId: callId,
+      amount: amountOfContracts,
+      assetData: bytes32(0)
+    });
+
+    transferBatch[1] = AccountStructs.AssetTransfer({
+      fromAcc: bobAcc,
+      toAcc: aliceAcc,
+      asset: cash,
+      subId: 0,
+      amount: premium,
+      assetData: bytes32(0)
+    });
+
+    // Bob transfers same option to Charlie for premium
+    transferBatch[2] = AccountStructs.AssetTransfer({
+      fromAcc: bobAcc,
+      toAcc: charlieAcc,
+      asset: option,
+      subId: callId,
+      amount: amountOfContracts,
+      assetData: bytes32(0)
+    });
+
+    transferBatch[3] = AccountStructs.AssetTransfer({
+      fromAcc: charlieAcc,
+      toAcc: bobAcc,
+      asset: cash,
+      subId: 0,
+      amount: premium,
+      assetData: bytes32(0)
+    });
+
+    accounts.submitTransfers(transferBatch, "");
+    (aliceBal, bobBal, charlieBal,) = _getAllCashBalances();
+    assertEq(uint(aliceBal), DEFAULT_DEPOSIT + uint(premium) - oiFee);
+    assertEq(uint(bobBal), DEFAULT_DEPOSIT);
+    assertEq(uint(charlieBal), DEFAULT_DEPOSIT - uint(premium) - oiFee);
+
+    // Settle OTM 
+    vm.warp(callExpiry);
+    int priceDecrease = 1000e18;
+    _setSpotPriceAndSubmitForExpiry(ETH_PRICE - priceDecrease, callExpiry);
+    pcrm.settleAccount(aliceAcc);
+    pcrm.settleAccount(bobAcc);
+    pcrm.settleAccount(charlieAcc);
+
+    // Balances remain the same as options expire worthless
+    (aliceBal, bobBal, charlieBal,) = _getAllCashBalances();
+    assertEq(uint(aliceBal), DEFAULT_DEPOSIT + uint(premium) - oiFee);
+    assertEq(uint(bobBal), DEFAULT_DEPOSIT);
+    assertEq(uint(charlieBal), DEFAULT_DEPOSIT - uint(premium) - oiFee);
+  }
+
+  function testThreeWayTradeOTMPut() public {
+    uint oiFee = (pcrm.OIFeeRateBPS()).multiplyDecimal(feed.getSpot());
+    int premium = 1000e18;
+
+    // ATM Put
+    uint putExpiry = block.timestamp + 4 weeks;
+    uint putStrike = 2000e18;
+    uint putId = option.getSubId(putExpiry, putStrike, false);
+
+    // Record pre balance
+    (int aliceBal, int bobBal, int charlieBal, int daveBal) = _getAllCashBalances();
+
+    AccountStructs.AssetTransfer[] memory transferBatch = new AccountStructs.AssetTransfer[](4);
+
+    // Alice transfer option to Bob for premium
+    transferBatch[0] = AccountStructs.AssetTransfer({
+      fromAcc: aliceAcc,
+      toAcc: bobAcc,
+      asset: option,
+      subId: putId,
+      amount: amountOfContracts,
+      assetData: bytes32(0)
+    });
+
+    transferBatch[1] = AccountStructs.AssetTransfer({
+      fromAcc: bobAcc,
+      toAcc: aliceAcc,
+      asset: cash,
+      subId: 0,
+      amount: premium,
+      assetData: bytes32(0)
+    });
+
+    // Bob transfers same option to Charlie for premium
+    transferBatch[2] = AccountStructs.AssetTransfer({
+      fromAcc: bobAcc,
+      toAcc: charlieAcc,
+      asset: option,
+      subId: putId,
+      amount: amountOfContracts,
+      assetData: bytes32(0)
+    });
+
+    transferBatch[3] = AccountStructs.AssetTransfer({
+      fromAcc: charlieAcc,
+      toAcc: bobAcc,
+      asset: cash,
+      subId: 0,
+      amount: premium,
+      assetData: bytes32(0)
+    });
+
+    accounts.submitTransfers(transferBatch, "");
+    (aliceBal, bobBal, charlieBal,) = _getAllCashBalances();
+    assertEq(uint(aliceBal), DEFAULT_DEPOSIT + uint(premium) - oiFee);
+    assertEq(uint(bobBal), DEFAULT_DEPOSIT);
+    assertEq(uint(charlieBal), DEFAULT_DEPOSIT - uint(premium) - oiFee);
+
+    // Settle OTM 
+    vm.warp(putExpiry);
+    int priceIncrease = 1000e18;
+    _setSpotPriceAndSubmitForExpiry(ETH_PRICE + priceIncrease, putExpiry);
+    pcrm.settleAccount(aliceAcc);
+    pcrm.settleAccount(bobAcc);
+    pcrm.settleAccount(charlieAcc);
+
+    // Balances remain the same as options expire worthless
+    (aliceBal, bobBal, charlieBal,) = _getAllCashBalances();
+    assertEq(uint(aliceBal), DEFAULT_DEPOSIT + uint(premium) - oiFee);
+    assertEq(uint(bobBal), DEFAULT_DEPOSIT);
+    assertEq(uint(charlieBal), DEFAULT_DEPOSIT - uint(premium) - oiFee);
+  }
+
   function testFourWayTradeITMCall() public {
     uint oiFee = (pcrm.OIFeeRateBPS()).multiplyDecimal(feed.getSpot());
 
