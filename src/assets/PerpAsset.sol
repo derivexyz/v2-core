738da45867411d1176f0a652285f3d5c6c2a57ba
Anton Cheng <antonassocareer@gmail.com>
2023-03-17 11:09:14 +0800

    chore: init sprint PR

---
+++ src/assets/PerpAsset.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "openzeppelin/token/ERC20/extensions/IERC20Metadata.sol";
+import "openzeppelin/token/ERC20/utils/SafeERC20.sol";
+import "openzeppelin/utils/math/SignedMath.sol";
+import "openzeppelin/utils/math/SafeCast.sol";
+import "lyra-utils/decimals/SignedDecimalMath.sol";
+import "lyra-utils/decimals/DecimalMath.sol";
+import "lyra-utils/ownership/Owned.sol";
+
+import "../interfaces/IAccounts.sol";
+import "../interfaces/ICashAsset.sol";
+
+/**
+ * @title PerpAsset
+ * @author Lyra
+ */
+
+contract CashAsset {}
