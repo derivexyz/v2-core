3fb0a4d18494212ee32e84fcbd859b4d1c26ade0
Anton Cheng <antonassocareer@gmail.com>
2023-03-28 10:53:34 +0800

    chore: init

---
+++ src/interfaces/IPerpManager.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "./IManager.sol";
+import "./AccountStructs.sol";
+
+interface IPerpManager is AccountStructs, IManager {
+  ///////////////
+  //   Errors  //
+  ///////////////
+
+  /// @dev Caller is not the Account contract
+  error PM_NotAccount();
+
+  /// @dev Not whitelist manager
+  error PM_NotWhitelistManager();
+
+  error PM_UnsupportedAsset(address asset); 
+  error PM_PortfolioBelowMargin(uint accountId, int margin);
+}
---
+++ src/risk-managers/PerpManager.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "openzeppelin/utils/math/SafeCast.sol";
+import "openzeppelin/utils/math/SignedMath.sol";
+
+import "lyra-utils/decimals/DecimalMath.sol";
+import "lyra-utils/decimals/SignedDecimalMath.sol";
+import "lyra-utils/encoding/OptionEncoding.sol";
+import "lyra-utils/ownership/Owned.sol";
+
+import "src/interfaces/IManager.sol";
+import "src/interfaces/IAccounts.sol";
+import "src/interfaces/ICashAsset.sol";
+import "src/interfaces/IPerpAsset.sol";
+import "src/interfaces/ISettlementFeed.sol";
+import "src/interfaces/IPerpManager.sol";
+
+import "forge-std/console2.sol";
+
+/**
+ * @title PerpManager
+ * @author Lyra
+ * @notice Risk Manager that controls transfer and margin requirements
+ */
+
+contract PerpManager is IPerpManager, Owned {
+  using SignedDecimalMath for int;
+  using DecimalMath for uint;
+  using SafeCast for uint;
+
+  ///////////////
+  // Variables //
+  ///////////////
+
+  /// @dev Account contract address
+  IAccounts public immutable accounts;
+
+  /// @dev Perp asset address
+  IPerpAsset public immutable perp;
+
+  /// @dev Cash asset address
+  ICashAsset public immutable cashAsset;
+
+  /// @dev Future feed oracle to get future price for an expiry
+  ISettlementFeed public immutable feed;
+
+  /// @dev Whitelisted managers. Account can only .changeManager() to whitelisted managers.
+  mapping(address => bool) public whitelistedManager;
+
+  ////////////////////////
+  //    Constructor     //
+  ////////////////////////
+
+  constructor(
+    IAccounts accounts_,
+    ICashAsset cashAsset_,
+    IPerpAsset perp_,
+    ISettlementFeed feed_
+  ) {
+    accounts = accounts_;
+    cashAsset = cashAsset_;
+    perp = perp_;
+    feed = feed_;
+  }
+
+  ////////////////////////
+  //    Admin-Only     //
+  ///////////////////////
+
+  /**
+   * @notice Whitelist or un-whitelist a manager used in .changeManager()
+   * @param _manager manager address
+   * @param _whitelisted true to whitelist
+   */
+  function setWhitelistManager(address _manager, bool _whitelisted) external onlyOwner {
+    whitelistedManager[_manager] = _whitelisted;
+  }
+
+  /**
+   * @notice Ensures asset is valid and Max Loss margin is met.
+   * @param accountId Account for which to check trade.
+   */
+  function handleAdjustment(uint accountId, uint tradeId, address, AssetDelta[] calldata assetDeltas, bytes memory)
+    public
+    override
+  {
+   
+  }
+
+  /**
+   * @notice Ensures new manager is valid.
+   * @param accountId Account for which to check trade.
+   * @param newManager IManager to change account to.
+   */
+  function handleManagerChange(uint accountId, IManager newManager) external view {
+    if (!whitelistedManager[address(newManager)]) {
+      revert PM_NotWhitelistManager();
+    }
+  }
+
+  //////////
+  // View //
+  //////////
+
+  
+}
