386455d2f778b76ba071d640c196ede57c112444
Anton Cheng <antonassocareer@gmail.com>
2023-04-04 10:08:01 +0800

    chore: remove _predictAddress function

--- test/integration-tests/shared/IntegrationTestBase.sol
+++ test/integration-tests/shared/IntegrationTestBase.sol
@@ -370,36 +370,6 @@ contract IntegrationTestBase is Test {
   }
 
   /**
-   * @dev predict the address of the next contract being deployed
-   */
-  function _predictAddress(address _origin, uint _nonce) public pure returns (address) {
-    if (_nonce == 0x00) {
-      return address(uint160(uint(keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), _origin, bytes1(0x80))))));
-    }
-    if (_nonce <= 0x7f) {
-      return address(uint160(uint(keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), _origin, uint8(_nonce))))));
-    }
-    if (_nonce <= 0xff) {
-      return address(
-        uint160(uint(keccak256(abi.encodePacked(bytes1(0xd7), bytes1(0x94), _origin, bytes1(0x81), uint8(_nonce)))))
-      );
-    }
-    if (_nonce <= 0xffff) {
-      return address(
-        uint160(uint(keccak256(abi.encodePacked(bytes1(0xd8), bytes1(0x94), _origin, bytes1(0x82), uint16(_nonce)))))
-      );
-    }
-    if (_nonce <= 0xffffff) {
-      return address(
-        uint160(uint(keccak256(abi.encodePacked(bytes1(0xd9), bytes1(0x94), _origin, bytes1(0x83), uint24(_nonce)))))
-      );
-    }
-    return address(
-      uint160(uint(keccak256(abi.encodePacked(bytes1(0xda), bytes1(0x94), _origin, bytes1(0x84), uint32(_nonce)))))
-    );
-  }
-
-  /**
    * for coverage to ignore
    */
   function test() public {}
