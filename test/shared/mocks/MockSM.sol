4d3f9fba9df4539a83d7a0b041e9ea9a3c9f6197
Anton Cheng <antonassocareer@gmail.com>
2023-05-03 13:03:46 +0800

    chore: cleanup path in mocks

--- test/shared/mocks/MockSM.sol
+++ test/shared/mocks/MockSM.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
-import "../../../src/interfaces/ISecurityModule.sol";
-import "../../../src/interfaces/IAccounts.sol";
-import "../../../src/interfaces/IManager.sol";
-import "../../../src/interfaces/IAsset.sol";
+import "src/interfaces/ISecurityModule.sol";
+import "src/interfaces/IAccounts.sol";
+import "src/interfaces/IManager.sol";
+import "src/interfaces/IAsset.sol";
 
 contract MockSM is ISecurityModule {
   IAccounts public immutable accounts;
