8e525627dcaa22c6689bcc03a9abbe6dad657779
Anton Cheng <antonassocareer@gmail.com>
2023-05-15 09:56:18 +0800

    chore: update typehash

--- src/feeds/LyraSpotFeed.sol
+++ src/feeds/LyraSpotFeed.sol
@@ -23,9 +23,8 @@ contract LyraSpotFeed is EIP712, Ownable2Step, ILyraSpotFeed, ISpotFeed, IDataRe
 
   mapping(address => bool) public isSigner;
 
-  bytes32 public constant SPOT_DATA_TYPEHASH = keccak256(
-    "SpotData(uint96 price,uint96 confidence,uint64 timestamp,uint deadline,address signer,bytes signature)"
-  );
+  bytes32 public constant SPOT_DATA_TYPEHASH =
+    keccak256("SpotData(uint96 price,uint96 confidence,uint64 timestamp,uint deadline,address signer,bytes signature)");
 
   ////////////////////////
   //    Constructor     //
