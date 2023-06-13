12f440901863e87657d4c61bb9497aade6f58b6b
Anton Cheng <antonassocareer@gmail.com>
2023-06-13 11:37:17 +0200

    chore: remove unnecessary test contracts

--- src/periphery/OptionSettlementHelper.sol
+++ src/periphery/OptionSettlementHelper.sol
@@ -6,6 +6,10 @@ import {IDataReceiver} from "src/interfaces/IDataReceiver.sol";
 import {ILiquidatableManager} from "src/interfaces/ILiquidatableManager.sol";
 import {IOption} from "src/interfaces/IOption.sol";
 
+/**
+ * @title OptionSettlementHelper
+ * @notice helper contract compliant with IDataReceiver interface, so we can settle options if necessary before running margin checks
+ */
 contract OptionSettlementHelper is IDataReceiver {
   function acceptData(bytes calldata data) external {
     (address manager, address option, uint accountId) = abi.decode(data, (address, address, uint));
--- src/periphery/PerpSettlementHelper.sol
+++ src/periphery/PerpSettlementHelper.sol
@@ -6,7 +6,14 @@ import {IDataReceiver} from "src/interfaces/IDataReceiver.sol";
 import {ILiquidatableManager} from "src/interfaces/ILiquidatableManager.sol";
 import {IPerpAsset} from "src/interfaces/IPerpAsset.sol";
 
+/**
+ * @title PerpSettlementHelper
+ * @notice helper contract compliant with IDataReceiver interface, so we can settle perps if necessary before running margin checks
+ */
 contract PerpSettlementHelper is IDataReceiver {
+  ///@dev just a random function so forge coverage won't confuse this with OptionSettlementHelper
+  string public name = "PerpSettlementHelper";
+
   function acceptData(bytes calldata data) external {
     (address manager, address perp, uint accountId) = abi.decode(data, (address, address, uint));
 
