d533fc020f4572da0bab22fc638cf13b0831ecf0
Anton Cheng <antonassocareer@gmail.com>
2023-06-27 11:04:47 +0800

    chore: cleanup _getConfig

--- scripts/utils.sol
+++ scripts/utils.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
+import {ConfigJson} from "./types.sol";
 
 contract Utils is Script {
 
   ///@dev read input from json 
   ///@dev standard path: scripts/input/{chainId}/{input}.json, as defined in 
   ////    https://book.getfoundry.sh/tutorials/best-practices?highlight=script#scripts
-  function readInput(string memory input) internal view returns (string memory) {
+  function _readInput(string memory input) internal view returns (string memory) {
     string memory inputDir = string.concat(vm.projectRoot(), "/scripts/input/");
     string memory chainDir = string.concat(vm.toString(block.chainid), "/");
     string memory file = string.concat(input, ".json");
@@ -16,7 +17,7 @@ contract Utils is Script {
   }
 
   /// @dev this should only be used to deploy mocks for local development
-  function writeToInput(string memory filename, string memory content) internal {
+  function _writeToInput(string memory filename, string memory content) internal {
     string memory inputDir = string.concat(vm.projectRoot(), "/scripts/input/");
     string memory chainDir = string.concat(vm.toString(block.chainid), "/");
     string memory file = string.concat(filename, ".json");
@@ -26,12 +27,20 @@ contract Utils is Script {
   }
 
   /// @dev use this function to write deployed contract address to deployments folder
-  function writeToDeployments(string memory filename, string memory content) internal {
+  function _writeToDeployments(string memory filename, string memory content) internal {
     string memory inputDir = string.concat(vm.projectRoot(), "/deployments/");
     string memory chainDir = string.concat(vm.toString(block.chainid), "/");
     string memory file = string.concat(filename, ".json");
     vm.writeJson(content, string.concat(inputDir, chainDir, file));
 
     console2.log("Written to deployment ", string.concat(inputDir, chainDir, file));
+  }
+
+  /// @dev get config from current chainId
+  function _getConfig() internal view returns (ConfigJson memory config) {
+    string memory file = _readInput("config");
+
+    bytes memory usdcAddrRaw = vm.parseJson(file);
+    config = abi.decode(usdcAddrRaw, (ConfigJson));
   }
 }
