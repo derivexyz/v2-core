f5962cdaf5ecd85caadfd902594d31a7b937dfb1
Anton Cheng <antonassocareer@gmail.com>
2023-07-24 14:48:06 +0800

    test: add revert test

--- test/risk-managers/unit-tests/PMRM/TestPMRM_Settle.t.sol
+++ test/risk-managers/unit-tests/PMRM/TestPMRM_Settle.t.sol
@@ -90,6 +90,22 @@ contract TestPMRM_Settlement is PMRMTestBase {
     assertEq(cashBefore - cashAfter, 500e18);
   }
 
+  function testCannotCallUnWhitelistedContractInProcessManagerData() public {
+    pmrm.setWhitelistedCallee(address(optionHelper), false);
+
+    _depositCash(aliceAcc, 2000e18);
+
+    IBaseManager.ManagerData[] memory allData = new IBaseManager.ManagerData[](1);
+    allData[0] = IBaseManager.ManagerData({receiver: address(optionHelper), data: ""});
+    bytes memory managerData = abi.encode(allData);
+
+    ISubAccounts.AssetTransfer memory transfer =
+      ISubAccounts.AssetTransfer({fromAcc: aliceAcc, toAcc: bobAcc, asset: cash, subId: 0, amount: 0, assetData: ""});
+
+    vm.expectRevert(IBaseManager.BM_UnauthorizedCall.selector);
+    subAccounts.submitTransfer(transfer, managerData);
+  }
+
   function testCanSettleOptionWithBadAddress() public {
     vm.expectRevert(IPMRM.PMRM_UnsupportedAsset.selector);
     pmrm.settleOptions(IOption(address(mockPerp)), aliceAcc);
