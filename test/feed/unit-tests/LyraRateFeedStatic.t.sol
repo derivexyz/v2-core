71bff531e25843be52ff44303d9316f827764ff7
Anton Cheng <antonassocareer@gmail.com>
2023-09-04 14:52:17 +0800

    build: add simple LyraRateFeedStatic feed contract for launch

---
+++ test/feed/unit-tests/LyraRateFeedStatic.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import "../../../src/feeds/LyraRateFeedStatic.sol";
+
+import "forge-std/Test.sol";
+
+contract UNIT_LyraRateFeed is Test {
+  LyraRateFeedStatic feed;
+
+  function setUp() public {
+    feed = new LyraRateFeedStatic();
+  }
+
+  function testSetRate() public {
+    int64 _rate = 0.04e18;
+    uint64 _confidence = 1e18;
+    feed.setRate(_rate, _confidence);
+
+    (int rate, uint confidence) = feed.getInterestRate(0);
+    assertEq(rate, _rate);
+    assertEq(confidence, _confidence);
+
+    feed.setRate(-_rate, _confidence);
+    (rate,) = feed.getInterestRate(0);
+    assertEq(rate, -_rate);
+  }
+
+  function testCannotSetRateOutOfRange() public {
+    int64 _rate = 1.1e18;
+    uint64 _confidence = 1e18;
+    vm.expectRevert(LyraRateFeedStatic.LRFS_StaticRateOutOfRange.selector);
+    feed.setRate(_rate, _confidence);
+  }
+}
