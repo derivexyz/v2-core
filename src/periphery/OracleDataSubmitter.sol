20e3a19e8a81945716ea2e2c4516a36151e134d5
Dominic Romanowski <romanowski.dominic@gmail.com>
2023-09-05 20:08:51 +1000

    add extra oracleDataSubmitter convenience contract

---
+++ src/periphery/OracleDataSubmitter.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import "../interfaces/IBaseManager.sol";
+import "../interfaces/IDataReceiver.sol";
+
+contract OracleDataSubmitter {
+  function submitData(bytes calldata managerData) external {
+    // parse array of data and update each oracle or take action
+    IBaseManager.ManagerData[] memory managerDatas = abi.decode(managerData, (IBaseManager.ManagerData[]));
+    for (uint i; i < managerDatas.length; i++) {
+      IDataReceiver(managerDatas[i].receiver).acceptData(managerDatas[i].data);
+    }
+  }
+}
