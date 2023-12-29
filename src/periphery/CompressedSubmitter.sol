fca10297d68bbafc1e73c6752821c4d213a10930
Anton Cheng <antonassocareer@gmail.com>
2023-12-29 17:53:07 +0800

    build: first improvement, remove array of ManagerData structure

---
+++ src/periphery/CompressedSubmitter.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.18;
+
+import "openzeppelin/access/Ownable2Step.sol";
+import "../interfaces/IBaseManager.sol";
+import "../interfaces/IDataReceiver.sol";
+
+import "forge-std/console2.sol";
+
+contract CompressedSubmitter is IDataReceiver, Ownable2Step {
+
+  mapping (uint8 => address) public feedIds;
+
+  event FeedIdRegistered(uint8 id, address feed);
+
+  /**
+   * @dev submit compressed data directly (not through the manager)
+   */
+  function submitCompressedData(bytes calldata data) external {
+
+    IBaseManager.ManagerData[] memory feedDatas = _parseFeedDataArray(data);
+
+    for (uint i; i < feedDatas.length; i++) {
+      IDataReceiver(feedDatas[i].receiver).acceptData(feedDatas[i].data);
+    }
+  }
+
+  /**
+   * @dev used as an "un-wrapper" for manager data to submit through the manager.
+   *      Data is compressed into bytes
+   */
+  function acceptData(bytes calldata data) external {
+    IBaseManager.ManagerData[] memory feedDatas = _parseFeedDataArray(data);
+
+    for (uint i; i < feedDatas.length; i++) {
+      IDataReceiver(feedDatas[i].receiver).acceptData(feedDatas[i].data);
+    }
+  }
+
+  /**
+   * Map ids to feed addresses
+   */
+  function registerFeedIds(uint8 id, address feed) external onlyOwner {
+    feedIds[id] = feed;
+
+    emit FeedIdRegistered(id, feed);
+  }
+
+
+  function _parseFeedDataArray(bytes calldata data) internal returns (IBaseManager.ManagerData[] memory) {
+    
+    bytes memory data2 = data[0:1];
+    console2.logBytes(data2);
+
+
+    // first byte of each byte array is number of feeds
+    uint8 numFeeds = sliceUint8(data, 1);
+
+    IBaseManager.ManagerData[] memory feedDatas = new IBaseManager.ManagerData[](numFeeds);
+
+    // how many bytes have been "used"
+    uint offset = 1;
+
+    for (uint i; i < numFeeds; i++) {
+      // 1 bytes of ID
+      uint8 feedId = sliceUint8(data, offset + 1);
+
+      // 4 bytes of data length
+      bytes calldata lengthData = data[offset + 1: offset + 5];
+      uint256 length = bytesToUint(lengthData);
+      
+      // [length] bytes of data
+      bytes calldata feedData = data[offset + 5: offset + 5 + length];
+
+      feedDatas[i] = IBaseManager.ManagerData({
+        receiver: feedIds[feedId],
+        data: feedData
+      });
+
+
+      offset += 5 + length;
+    }
+
+    return feedDatas;
+  }
+
+  /// read a single byte and return it as a uint8
+  function sliceUint8(bytes memory bs, uint location) internal pure returns (uint8 x) {
+    assembly {
+        x := mload(add(bs, location))
+    }
+  }
+
+  // read 4 bytes and return as uint32
+  function sliceUint32(bytes memory bs, uint location) internal pure returns (uint32 x) {
+    assembly {
+        x := mload(add(bs, location))
+    }
+  }
+
+  function bytesToUint(bytes memory b) internal pure returns (uint num){
+    for(uint i=0;i<b.length;i++){
+        num = num + uint(uint8(b[i]))*(2**(8*(b.length-(i+1))));
+    }
+  }
+}
--- test/feed/integration-tests/CompressedSubmitter.sol
+++ test/feed/integration-tests/CompressedSubmitter.sol
@@ -1,43 +1,61 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.18;
 
-import "../../../src/periphery/OracleDataSubmitter.sol";
+import "../../../src/periphery/CompressedSubmitter.sol";
 import "../../../src/feeds/LyraSpotFeed.sol";
 import "../unit-tests/LyraFeedTestUtils.sol";
 import {IBaseManager} from "../../../src/interfaces/IBaseManager.sol";
 
-contract OracleDataSubmitterTest is LyraFeedTestUtils {
+contract CompressedSubmitterTest is LyraFeedTestUtils {
   uint pk = 0xBEEF;
 
-  OracleDataSubmitter submitter;
+  CompressedSubmitter submitter;
   LyraSpotFeed spotFeed1;
   LyraSpotFeed spotFeed2;
 
+  uint8 feedId1 = 1;
+  uint8 feedId2 = 2;
+
+
   function setUp() public {
-    submitter = new OracleDataSubmitter();
+    submitter = new CompressedSubmitter();
     spotFeed1 = new LyraSpotFeed();
     spotFeed2 = new LyraSpotFeed();
 
     spotFeed1.addSigner(vm.addr(pk), true);
     spotFeed2.addSigner(vm.addr(pk), true);
+
+    submitter.registerFeedIds(feedId1, address(spotFeed1));
+    submitter.registerFeedIds(feedId2, address(spotFeed2));
   }
 
   function testSubmitBatchData() public {
+
+    // prepare raw data
     IBaseLyraFeed.FeedData memory spotData1 = _getDefaultSpotData();
     bytes memory data1 = _signFeedData(spotFeed1, pk, spotData1);
+
 
     IBaseLyraFeed.FeedData memory spotData2 = _getDefaultSpotData();
     bytes memory data2 = _signFeedData(spotFeed2, pk, spotData2);
 
-    IBaseManager.ManagerData[] memory managerDatas = new IBaseManager.ManagerData[](2);
-    managerDatas[0] = IBaseManager.ManagerData(address(spotFeed1), data1);
-    managerDatas[1] = IBaseManager.ManagerData(address(spotFeed2), data2);
-    bytes memory managerData = abi.encode(managerDatas);
+    uint32 data1Length = uint32(data1.length);
+    uint32 data2Length = uint32(data2.length);
 
-    // console2.log("managerData");
-    // console2.logBytes(managerData);
+    uint8 numOfFeeds = 2;
 
-    submitter.submitData(managerData);
+    // bytes[] memory managerDatas = new bytes[](2);
+    bytes memory compressedData = abi.encodePacked(
+      numOfFeeds,
+      feedId1,
+      data1Length,
+      data1,
+      feedId2,
+      data2Length,
+      data2
+    );
+    
+    submitter.submitCompressedData(compressedData);
 
     (uint spot1, uint confidence1) = spotFeed1.getSpot();
     (uint spot2, uint confidence2) = spotFeed2.getSpot();
