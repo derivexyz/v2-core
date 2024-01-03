6e74c4e6f6bf0be5100bd54c5311b1ec00b55a3e
Anton Cheng <antonassocareer@gmail.com>
2024-01-03 15:43:04 +0800

    chore: fmt

--- src/l2/LyraERC20.sol
+++ src/l2/LyraERC20.sol
@@ -9,40 +9,39 @@ import "openzeppelin/access/Ownable2Step.sol";
  * @author Lyra
  * @notice ERC20 token on Lyra Chain
  */
-contract LyraERC20 is Ownable2Step, ERC20 { 
+contract LyraERC20 is Ownable2Step, ERC20 {
+  uint8 internal _decimals;
 
-    uint8 internal _decimals;
+  mapping(address => bool) public minters;
 
-    mapping(address => bool) public minters;
+  error OnlyMinter();
 
-    error OnlyMinter();
+  modifier onlyMinter() {
+    if (!minters[msg.sender]) revert OnlyMinter();
+    _;
+  }
 
-    modifier onlyMinter() {
-        if(!minters[msg.sender]) revert OnlyMinter();
-        _;
-    }
+  event MinterConfigured(address indexed minter, bool enabled);
 
-    event MinterConfigured(address indexed minter, bool enabled);
-    
-    constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20(name_, symbol_) {
-        _decimals = decimals_;
-    }
+  constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20(name_, symbol_) {
+    _decimals = decimals_;
+  }
 
-    function decimals() public view virtual override returns (uint8) {
-        return _decimals;
-    }
+  function decimals() public view virtual override returns (uint8) {
+    return _decimals;
+  }
 
-    function configureMinter(address minter, bool enabled) public onlyOwner {
-        minters[minter] = enabled;
+  function configureMinter(address minter, bool enabled) public onlyOwner {
+    minters[minter] = enabled;
 
-        emit MinterConfigured(minter, enabled);
-    }
+    emit MinterConfigured(minter, enabled);
+  }
 
-    function mint(address to, uint256 amount) public onlyMinter {
-        _mint(to, amount);
-    }
+  function mint(address to, uint amount) public onlyMinter {
+    _mint(to, amount);
+  }
 
-    function burn(address from, uint256 amount) public onlyMinter {
-        _burn(from, amount);
-    }
+  function burn(address from, uint amount) public onlyMinter {
+    _burn(from, amount);
+  }
 }
--- test/misc/LyraERC20.t.sol
+++ test/misc/LyraERC20.t.sol
@@ -8,37 +8,37 @@ import "forge-std/Test.sol";
 import {LyraERC20} from "../../src/l2/LyraERC20.sol";
 
 contract UNIT_LyraERC20_Test is Test {
-    LyraERC20 public usdc;
+  LyraERC20 public usdc;
 
-    function setUp() public {
-        usdc = new LyraERC20("USDC", "USDC", 6);
-    }
+  function setUp() public {
+    usdc = new LyraERC20("USDC", "USDC", 6);
+  }
 
-    function testConfigureMinter() public {
-        usdc.configureMinter(address(this), true);
-        assertTrue(usdc.minters(address(this)));
-        usdc.configureMinter(address(this), false);
-        assertFalse(usdc.minters(address(this)));
-    } 
+  function testConfigureMinter() public {
+    usdc.configureMinter(address(this), true);
+    assertTrue(usdc.minters(address(this)));
+    usdc.configureMinter(address(this), false);
+    assertFalse(usdc.minters(address(this)));
+  }
 
-    function testMintAndBurn() public {
-        usdc.configureMinter(address(this), true);
-        usdc.mint(address(this), 100);
-        assertEq(usdc.balanceOf(address(this)), 100);
+  function testMintAndBurn() public {
+    usdc.configureMinter(address(this), true);
+    usdc.mint(address(this), 100);
+    assertEq(usdc.balanceOf(address(this)), 100);
 
-        usdc.burn(address(this), 50);
-        assertEq(usdc.balanceOf(address(this)), 50);
-    }
+    usdc.burn(address(this), 50);
+    assertEq(usdc.balanceOf(address(this)), 50);
+  }
 
-    function testCanOnlyConfigureMinterByOwner() public {
-        vm.prank(address(0xaa));
-        vm.expectRevert(bytes("Ownable: caller is not the owner"));
-        usdc.configureMinter(address(this), true);
-    }
+  function testCanOnlyConfigureMinterByOwner() public {
+    vm.prank(address(0xaa));
+    vm.expectRevert(bytes("Ownable: caller is not the owner"));
+    usdc.configureMinter(address(this), true);
+  }
 
-    function testCanOnlyMintByMinter() public {
-        vm.prank(address(0xaa));
-        vm.expectRevert(LyraERC20.OnlyMinter.selector);
-        usdc.mint(address(this), 100);
-    }
+  function testCanOnlyMintByMinter() public {
+    vm.prank(address(0xaa));
+    vm.expectRevert(LyraERC20.OnlyMinter.selector);
+    usdc.mint(address(this), 100);
+  }
 }
