820fabb74040dd52a41388cde53463d8ab9d9b24
Dominic Romanowski <romanowski.dominic@gmail.com>
2024-02-14 15:43:26 +1100

    add borrow test

--- test/feed/integration-tests/SFPSpotFeed.t.sol
+++ test/feed/integration-tests/SFPSpotFeed.t.sol
@@ -1,33 +1,56 @@
+pragma solidity ^0.8.18;
+
 import "forge-std/Test.sol";
 import "src/feeds/SFPSpotFeed.sol";
 import "./sfp-contracts/StrandsAPI.sol";
 import "./sfp-contracts/StrandsSFP.sol";
+import {
+  IntegrationTestBase, IStandardManager, Config, WrappedERC20Asset, IForwardFeed, IVolFeed, ISpotFeed, IManager
+} from "../../integration-tests/shared/IntegrationTestBase.t.sol";
 
 
-contract SFPTest is Test {
+contract SFPTest is IntegrationTestBase {
   address deployer;
-  address alice;
 
   StrandsAPI strandsAPI;
   StrandsSFP strandsSFP;
   SFPSpotFeed sfpSpotFeed;
 
-  function setUp() public {
-    deployer = address(this);
-    alice = address(0xa);
+  WrappedERC20Asset wrappedSFP;
 
-    strandsAPI = new StrandsAPI(deployer, deployer);
+  function setUp() public {
+    _setupIntegrationTestComplete();
+
+    strandsAPI = new StrandsAPI(address(this), address(this));
     strandsSFP = new StrandsSFP(strandsAPI);
     sfpSpotFeed = new SFPSpotFeed(IStrandsSFP(address(strandsSFP)));
+
+    wrappedSFP = new WrappedERC20Asset(subAccounts, IERC20Metadata(strandsSFP));
+
+    wrappedSFP.setWhitelistManager(address(srm), true);
+    wrappedSFP.setTotalPositionCap(srm, 1000e18);
+
+    uint marketId = srm.createMarket("SFP");
+
+    srm.whitelistAsset(wrappedSFP, marketId, IStandardManager.AssetType.Base);
+    srm.setOraclesForMarket(marketId, ISpotFeed(sfpSpotFeed), IForwardFeed(address(0)), IVolFeed(address(0)));
+
+    (
+      ,,,
+      IStandardManager.BaseMarginParams memory baseMarginParams
+    ) = Config.getSRMParams();
+
+    srm.setBaseAssetMarginFactor(marketId, 0.95e18, 0.9475e18);
+    srm.setBorrowingEnabled(true);
+
+    // setup accounts so some cash exists to borrow
+    _setupAliceAndBob();
+    _depositCash(bob, bobAcc, 10000e18);
   }
 
 
-  function test_sfp() public {
-    strandsAPI.mint(alice, 100e18);
-    vm.prank(alice);
-    strandsAPI.approve(address(strandsSFP), 100e18);
-    vm.prank(alice);
-    strandsSFP.deposit(100e18, alice);
+  function test_sfpSpotFeed() public {
+    _mintSFP(alice, 100e18);
 
     assertEq(strandsSFP.balanceOf(alice), 100e18);
     assertEq(strandsSFP.totalSupply(), 100e18);
@@ -40,6 +63,28 @@ contract SFPTest is Test {
     (spot, confidence) = sfpSpotFeed.getSpot();
     assertApproxEqAbs(spot, 1.01e18, 0.00001e18);
     assertEq(confidence, 1e18);
+  }
 
+  function test_borrowingAgainstSFP() public {
+    _mintSFP(alice, 100e18);
+    vm.startPrank(alice);
+    uint subAcc = subAccounts.createAccount(alice, srm);
+    strandsSFP.approve(address(wrappedSFP), 100e18);
+    wrappedSFP.deposit(subAcc, 100e18);
+
+    // 6 decimals for USDC
+    cash.withdraw(subAcc, 50e6, alice);
+    vm.stopPrank();
+
+    assertEq(usdc.balanceOf(alice), 50e6);
+
+  }
+
+  function _mintSFP(address user, uint amount) internal {
+    strandsAPI.mint(user, amount);
+    vm.startPrank(user);
+    strandsAPI.approve(address(strandsSFP), amount);
+    strandsSFP.deposit(amount, user);
+    vm.stopPrank();
   }
 }
